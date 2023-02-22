74f78754c7197c8194342102732016245b62f409
Robert Shepley <shepleysound@gmail.com>
2023-02-22 02:41:47 -0800

    Check for slot type on landing pages.

--- components/pageTypes/LandingPage.tsx
+++ components/pageTypes/LandingPage.tsx
@@ -5,9 +5,12 @@ export default function LandingPage({ content }) {
   return (
     <Layout>
       <div className="mx-auto max-w-7xl lg:px-6 xl:px-8">
-        {content.slots.map((slot) => (
-          <TextWithIllustration key={slot._key} {...slot} />
-        ))}
+        {content?.slots?.map(
+          (slot) =>
+            slot._type === 'textWithIllustration' && (
+              <TextWithIllustration key={slot._key} {...slot} />
+            )
+        )}
       </div>
     </Layout>
   )
