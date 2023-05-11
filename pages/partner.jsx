a0770a8fb441212b45c15a2af9eb7389d2f4b190
Misty Becker <mistyb01@uw.edu>
2023-05-11 14:55:48 -0700

    title and buttonText are optional props for CardOneIcon

--- pages/partner.jsx
+++ pages/partner.jsx
@@ -7,7 +7,7 @@ import TextAreaFormGroup from '../components/form/TextAreaFormGroup'
 import TextFormGroup from '../components/form/TextFormGroup'
 import TextFormGroupWithAddon from '../components/form/TextFormGroupWithAddon'
 import Layout from '../components/Layout'
-import CardIcon from '../components/cards/CardIcon'
+import CardOneIcon from '../components/cards/CardOneIcon'
 import Stack from '@mui/material/Stack'
 
 import AccessibilityIcon from '@mui/icons-material/Accessibility';
@@ -54,18 +54,20 @@ export default function PartnerForm() {
               <div>
                 {/* TEST CODE FOR CARD 1! */}
                 <Stack direction="row">
-                  <CardIcon
+                  <CardOneIcon
                     title="Excellence"
                     description="Striving for professional excellence means taking an uncompromising approach to the service we endeavor to provide. We ensure the utmost quality in what we deliver."
                     icon={<AccessibilityIcon fontSize="large" sx={{color:"white"}}/>}/>
-                  <CardIcon
+                  <CardOneIcon
                     title="Efficacy"
                     description="What we do will have impact. We will apply the pareto principle (and other frameworks) to ensure that we are optimizing our efforts at every step in our process from engagement to delivery."
                     icon={<AllInclusiveIcon fontSize="large" sx={{color:"white"}}/>}/>
-                  <CardIcon
-                    title="Efficiency"
+                  <CardOneIcon
+                    // title="Efficiency"
                     description="We work with an eye toward maintaining a steady and speedy cadence whenever possible. We don't sacrifice quality, but we work with an MLP (minimum loveable product) mindset."
-                    icon={<AddTaskIcon fontSize="large" sx={{color:"white"}}/>}/>
+                    icon={<AddTaskIcon fontSize="large" sx={{color:"white"}}/>}
+                    buttonText="Volunteer with us"/>
+                    
                 </Stack>
                 {/* END OF TEST CODE */}
                 <h3 className="text-lg font-medium leading-6">
