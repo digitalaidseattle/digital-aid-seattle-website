9ba4bc5962edb7b216a68fd5aa5dff22c63d58ab
Weston Norwood <weston.norwood@gmail.com>
2023-05-12 12:11:32 -0700

    Revert "feat: Implement MUI theme with basic styles"
    This reverts commit add885f98a9e1449a19ad546ff171bfa507f0284.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,45 @@
 import 'styles/global.css'
 
-import { ThemeProvider } from '@mui/material'
+import { Lato, League_Gothic } from '@next/font/google'
+import { IBM_Plex_Mono, Inter, PT_Serif } from '@next/font/google'
 import { AppProps } from 'next/app'
-import { theme } from 'theme/theme'
+
+const lato = Lato({
+  variable: '--font-lato',
+  display: 'swap',
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700', '900'],
+  style: ['normal', 'italic'],
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+})
+
+const leagueGothic = League_Gothic({
+  variable: '--font-league-gothic',
+  display: 'swap',
+  subsets: ['latin'],
+  style: 'normal',
+  weight: ['400'],
+})
+
+// const serif = PT_Serif({
+//   variable: '--font-serif',
+//   style: ['normal', 'italic'],
+//   subsets: ['latin'],
+//   weight: ['400', '700'],
+// })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <style jsx global>
+        {`
+          :root {
+            --font-lato: ${lato.style.fontFamily};
+            --font-league-gothic: ${leagueGothic.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
     </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Oswald:wght@400;500&display=swap"
-        />
-      </Head>
+      <Head />
       <body className="bg-gray-100 text-black dark:bg-gray-700 dark:text-gray-100">
         <Main />
         <NextScript />
