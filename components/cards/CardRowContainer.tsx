47c21a9e5411663bdde2dc40a54a76145d25abb2
Misty Becker <mistyb01@uw.edu>
2023-05-12 20:04:09 -0700

    responsive CardRowContainer

--- components/cards/CardRowContainer.tsx
+++ components/cards/CardRowContainer.tsx
@@ -7,8 +7,9 @@ type Props = {
 
 const CardRowContainer = ( {children} : Props ) => {
     return (
-        // margins for testing purposes
-        <Stack direction="row" spacing={4} sx={{ 
+        // margins for testing purposes, feel free to remove
+        <Stack direction={{xs: "column", sm: "row"}} spacing={4} sx={{
+            flexWrap: 'wrap',
             // mr: '200px',
             // ml: '200px', 
             // mt: '100px',
@@ -17,5 +18,6 @@ const CardRowContainer = ( {children} : Props ) => {
         </Stack>
     )
 }
+// TODO: a mobile friendly version
 
 export default CardRowContainer;
