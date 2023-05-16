36e68b4ecc84e1e5ab832630018e0b97f9616f56
Santina Lin <santina.lin@convoy.com>
2023-05-16 08:58:29 -0700

    Initial header design

---
+++ components/OSDesign/CommonHeader.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable jsx-a11y/alt-text */
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import { theme } from 'theme/theme'
+
+import OSLogo from "../../assets/darkThemeLogo.svg"
+
+const pages = ['About', 'Projects', 'Partner', 'Volunteer', 'Events'];
+
+function CommonHeader() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static"  sx={{background: theme.palette.primary.main}}>
+      <Container maxWidth="xl">
+        <Toolbar sx={{height: '50px'}}>
+            {/** TODO fix this - eslint-disable-next-line @next/next/no-img-element */}
+            <img
+                src={OSLogo.src}
+                style={{ height: '80%', width: '120px', margin: '0px 60px 0px 0px' }}
+            />
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="page-info"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu} style={{borderRadius: '0px'}}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ color: 'white', borderRadius: '0px'}}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default CommonHeader;
