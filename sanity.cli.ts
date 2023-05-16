9d7717715466c648f839f71a9cd77591d3713ebd
Santina Lin <santina.lin@convoy.com>
2023-05-16 16:46:40 -0700

    fix sanity client error

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,7 @@
 import { loadEnvConfig } from '@next/env'
 import { defineCliConfig } from 'sanity/cli'
-import sanityClient from '@sanity/client'
+
+import { createClient } from 'next-sanity'
 
 const dev = process.env.NODE_ENV !== 'production'
 loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
@@ -8,10 +9,11 @@ loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
 // @TODO report top-level await bug
 // Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
 // const { projectId, dataset } = await import('lib/sanity.api')
+
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export const client = sanityClient({
+export const client = createClient({
   projectId,
   dataset,
   apiVersion: '2023-05-12',
