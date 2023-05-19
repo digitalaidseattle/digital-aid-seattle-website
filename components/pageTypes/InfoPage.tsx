8392a76910818ad8deec523d25f46fdf50b9c708
Weston Norwood <weston.norwood@gmail.com>
2023-05-19 01:17:44 -0700

    fix: Address dark mode text contrast issue

--- components/pageTypes/InfoPage.tsx
+++ components/pageTypes/InfoPage.tsx
@@ -9,7 +9,7 @@ export default function InfoPage({ content }) {
       <div className="circuit-pattern absolute top-0 left-0 -z-50 h-full w-full" />
       <div className="relative h-full">
         <div className="max-7xl mx-auto h-full">
-          <div className="relative mx-auto h-full max-w-7xl bg-gray-100 px-6 py-16 lg:px-8">
+          <div className="relative mx-auto h-full max-w-7xl bg-gray-100 text-black dark:bg-gray-700 dark:text-gray-100 px-6 py-16 lg:px-8">
             <article className="prose prose-lg z-10 mx-auto max-w-prose prose-h1:text-center dark:prose-invert">
               <PortableText value={content.content} components={components} />
             </article>
