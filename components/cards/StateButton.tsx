8d4f7864e4d85ac034120a46fc997611e7fe8906
Misty Becker <mistyb01@uw.edu>
2023-06-02 14:50:08 -0700

    feat: Created ProjectCard, StateButton, and CardGridContainer components

---
+++ components/cards/StateButton.tsx
@@ -0,0 +1,35 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography/Typography'
+
+type StateButtonProps = {
+  state: 'active' | 'recruiting' | 'complete'
+}
+
+const StateButton = ({ state }: StateButtonProps) => {
+  const bgColor =
+    state === 'active'
+      ? '#FFFAD8'
+      : state === 'recruiting'
+      ? '#DDFFAF'
+      : '#FFF0EE'
+  return (
+    <Box
+      sx={{
+        color: '#343D3E',
+        textTransform: 'capitalize',
+        padding: '10px 12px',
+        borderRadius: '100px',
+        backgroundColor: bgColor,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Typography variant="titleSmall" sx={{ fontWeight: '600' }}>
+        {state}
+      </Typography>
+    </Box>
+  )
+}
+
+export default StateButton
