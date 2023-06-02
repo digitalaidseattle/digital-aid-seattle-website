0e9ac95a27bb14b83f021676bfa50a7e943ac811
Misty Becker <mistyb01@uw.edu>
2023-06-02 14:26:54 -0700

    feat: ProjectCard, StateButton and CardGridContainer components

---
+++ components/cards/CardProject.tsx
@@ -0,0 +1,74 @@
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import StateButton from './StateButton'
+import Placeholder from '../../assets/placeholder-img.png'
+
+
+type CardProjectProps = {  
+  title: string
+  partner: string
+  programAreas: string[]
+  description: string
+  status: "active" | "recruiting" | "complete"
+  projectLink: string
+  duration: {start: string, end: string} // unsure how to handle dates at this point
+}
+
+
+const CardProject = 
+({title, partner, programAreas, description, status, projectLink, duration}: CardProjectProps) => {
+    
+  
+      
+    return (
+        <Card>
+            <CardContent sx={{padding: '2rem', display: 'flex', flexDirection: 'column', gap: '1.5rem'}}>
+                <Box sx={{
+                    width: '100%',
+                    height: '0',
+                    border: '2px solid #EAF1F1',
+                    display: {xs: 'none', md: 'block'}
+                }}>
+                    {/* TODO: use an image prop so we can customize the image for different cards. below is just a placeholder for testing purposes. */}                   
+                    <img
+                    src={Placeholder.src}
+                    style={{
+                        height: '100%',
+                        width: '100%',
+                        borderRadius: '8px',
+                    }}
+                    />
+                </Box>
+                <Stack spacing="1rem">
+                    <Typography variant="titleLarge">{title}</Typography>
+                    <Typography variant="labelLarge">{partner}</Typography>
+                    <Typography variant="labelMedium">{programAreas.join(', ')}</Typography>
+                </Stack>
+                <Typography variant="bodyMedium">{description}</Typography>
+                <Stack 
+                    direction={{xs: 'column-reverse', sm: 'row'}} 
+                    alignItems={{xs: "auto", sm: "flex-end"}}
+                    justifyContent="space-between"
+                    spacing="1rem">
+                    <Button variant="contained" href={projectLink}>View Project</Button>
+                    <Stack 
+                        direction={{xs: 'row-reverse', sm: 'column'}} 
+                        justifyContent={{xs: "space-between", sm: 'auto'}}
+                        alignItems={{xs: "center", sm: 'auto'}}
+                        spacing="1rem"
+                        sx={{ paddingLeft: {xs:'2rem', sm: '0'} }}>
+                        <StateButton state={status}/>
+                        <Typography variant="labelMedium">{duration.start} - {duration.end}</Typography>
+                    </Stack>
+                </Stack>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default CardProject;
