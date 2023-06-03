89764c548c323c490de45b9be4bc8874f506839e
Santina Lin <santina.lin@convoy.com>
2023-06-03 09:53:52 -0700

    Move it one level up

--- components/CommonHeader.tsx
+++ components/CommonHeader.tsx
@@ -1,4 +1,6 @@
-/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/alt-text  */
+/* eslint-disable @next/next/no-img-element */
+import MenuIcon from '@mui/icons-material/Menu'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
@@ -11,11 +13,12 @@ import Typography from '@mui/material/Typography'
 import * as React from 'react'
 import { theme } from 'theme/theme'
 
-import OSLogo from '../../assets/darkThemeLogo.svg'
+import OSLogo from '../assets/darkThemeLogo.svg'
 
 const pages = ['About', 'Projects', 'Partner', 'Volunteer', 'Events']
 
 function CommonHeader() {
+  // React states and functions for handling the hamburger menu.
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -30,7 +33,7 @@ function CommonHeader() {
     <AppBar position="static" sx={{ background: theme.palette.primary.main }}>
       <Container maxWidth="xl">
         <Toolbar sx={{ height: '50px' }}>
-          {/** TODO fix this - eslint-disable-next-line @next/next/no-img-element */}
+          {/* LOGO */}
           <img
             src={OSLogo.src}
             style={{
@@ -39,6 +42,7 @@ function CommonHeader() {
               margin: '0px 60px 0px 0px',
             }}
           />
+          {/* Hamburger menu when the screen is small. */}
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
@@ -46,8 +50,10 @@ function CommonHeader() {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
-            ></IconButton>
+              sx={{ color: theme.palette.primary.contrastText }}
+            >
+              <MenuIcon />
+            </IconButton>
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
@@ -77,12 +83,17 @@ function CommonHeader() {
               ))}
             </Menu>
           </Box>
+          {/* Menu items that are shown on desktop */}
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ color: 'white', borderRadius: '0px' }}
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                  borderRadius: '0px',
+                }}
+                disableRipple={true}
               >
                 {page}
               </Button>
