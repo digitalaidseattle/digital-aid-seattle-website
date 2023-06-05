146d03b3f67363090cbb3085f35ea3c9da44d197
Misty Becker <mistyb01@uw.edu>
2023-06-05 09:14:18 -0700

    fix: removed test code

--- components/cards/CardProject.tsx
+++ components/cards/CardProject.tsx
@@ -27,67 +27,65 @@ const CardProject = ({
   projectLink,
   duration,
 }: CardProjectProps) => {
-  return (
-    <Card>
-      <CardContent
+  <Card>
+    <CardContent
+      sx={{
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1.5rem',
+      }}
+    >
+      <Box
         sx={{
-          padding: '2rem',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1.5rem',
+          width: '100%',
+          height: '0',
+          border: '2px solid #EAF1F1',
+          display: { xs: 'none', md: 'block' },
         }}
       >
-        <Box
-          sx={{
+        {/* TODO: use an image prop so we can customize the image for different cards. below is just a placeholder for testing purposes. */}
+        <img
+          src={Placeholder.src}
+          style={{
+            height: '100%',
             width: '100%',
-            height: '0',
-            border: '2px solid #EAF1F1',
-            display: { xs: 'none', md: 'block' },
+            borderRadius: '8px',
           }}
+        />
+      </Box>
+      <Stack spacing="1rem">
+        <Typography variant="titleLarge">{title}</Typography>
+        <Typography variant="labelLarge">{partner}</Typography>
+        <Typography variant="labelMedium">
+          {programAreas.join(', ')}
+        </Typography>
+      </Stack>
+      <Typography variant="bodyMedium">{description}</Typography>
+      <Stack
+        direction={{ xs: 'column-reverse', sm: 'row' }}
+        alignItems={{ xs: 'auto', sm: 'flex-end' }}
+        justifyContent="space-between"
+        spacing="1rem"
+      >
+        <Button variant="contained" href={projectLink}>
+          View Project
+        </Button>
+        <Stack
+          direction={{ xs: 'row-reverse', sm: 'column' }}
+          justifyContent={{ xs: 'space-between', sm: 'auto' }}
+          alignItems={{ xs: 'center', sm: 'auto' }}
+          spacing="1rem"
+          sx={{ paddingLeft: { xs: '2rem', sm: '0' } }}
         >
-          {/* TODO: use an image prop so we can customize the image for different cards. below is just a placeholder for testing purposes. */}
-          <img
-            src={Placeholder.src}
-            style={{
-              height: '100%',
-              width: '100%',
-              borderRadius: '8px',
-            }}
-          />
-        </Box>
-        <Stack spacing="1rem">
-          <Typography variant="titleLarge">{title}</Typography>
-          <Typography variant="labelLarge">{partner}</Typography>
+          <StateButton state={status} />
           <Typography variant="labelMedium">
-            {programAreas.join(', ')}
+            {duration.start} - {duration.end}
           </Typography>
         </Stack>
-        <Typography variant="bodyMedium">{description}</Typography>
-        <Stack
-          direction={{ xs: 'column-reverse', sm: 'row' }}
-          alignItems={{ xs: 'auto', sm: 'flex-end' }}
-          justifyContent="space-between"
-          spacing="1rem"
-        >
-          <Button variant="contained" href={projectLink}>
-            View Project
-          </Button>
-          <Stack
-            direction={{ xs: 'row-reverse', sm: 'column' }}
-            justifyContent={{ xs: 'space-between', sm: 'auto' }}
-            alignItems={{ xs: 'center', sm: 'auto' }}
-            spacing="1rem"
-            sx={{ paddingLeft: { xs: '2rem', sm: '0' } }}
-          >
-            <StateButton state={status} />
-            <Typography variant="labelMedium">
-              {duration.start} - {duration.end}
-            </Typography>
-          </Stack>
-        </Stack>
-      </CardContent>
-    </Card>
-  )
+      </Stack>
+    </CardContent>
+  </Card>
 }
 
 export default CardProject
