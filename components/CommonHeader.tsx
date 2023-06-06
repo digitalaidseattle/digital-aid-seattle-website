d7986053dc58f90cd605a37f14aa04acc3bcafc6
Santina Lin <santina.lin@convoy.com>
2023-06-06 06:13:49 -0700

    Header component (#19)
    * draft new header
    
    * Move it one level up
    
    * add missing package
    
    * slight fix to style
    
    * center the header content
    
    * Make hover over underlined

---
+++ components/CommonHeader.tsx
@@ -0,0 +1,131 @@
+/* eslint-disable jsx-a11y/alt-text  */
+/* eslint-disable @next/next/no-img-element */
+import MenuIcon from '@mui/icons-material/Menu'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+import { theme } from 'theme/theme'
+
+import OSLogo from '../assets/darkThemeLogo.svg'
+
+const pages = ['About', 'Projects', 'Partner', 'Volunteer', 'Events']
+
+const CommonHeader = () => {
+  // React states and functions for handling the hamburger menu.
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  return (
+    <AppBar position="static" sx={{ background: theme.palette.primary.main }}>
+      <Container maxWidth="xl">
+        <Toolbar sx={{ height: '50px' }}>
+          {/* Hamburger menu when the screen is small. */}
+          <Box
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            justifyContent="center"
+          >
+            {/* LOGO */}
+            <img
+              src={OSLogo.src}
+              style={{
+                height: '80%',
+                width: '120px',
+                margin: '0px 60px 0px 0px',
+              }}
+            />
+            <IconButton
+              size="large"
+              aria-label="page-info"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              sx={{ color: theme.palette.primary.contrastText }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  style={{ borderRadius: '0px' }}
+                >
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* Menu items that are shown on desktop */}
+          <Box
+            sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+            justifyContent="center"
+          >
+            {/* LOGO */}
+            <img
+              src={OSLogo.src}
+              style={{
+                height: '80%',
+                width: '120px',
+                margin: '0px 60px 0px 0px',
+              }}
+            />
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                  borderRadius: '0px',
+                }}
+                disableRipple={true}
+              >
+                <Link
+                  underline="hover"
+                  sx={{
+                    color: theme.palette.primary.contrastText,
+                    'text-underline-offset': '0.5rem',
+                  }}
+                >
+                  {page}
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
+}
+export default CommonHeader
