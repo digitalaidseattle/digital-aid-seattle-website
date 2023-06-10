398d07cc36912be6e01c6d347e28afb52a01b253
Misty Becker <mistyb01@uw.edu>
2023-06-10 05:54:01 -0700

    feat: search bar is toggleable

--- components/filters/SearchToggle.tsx
+++ components/filters/SearchToggle.tsx
@@ -7,22 +7,29 @@ const SearchToggle = () => {
     const [displayInput, setDisplayInput] = useState(false);
     return (
         <>
+    
         <Box color="primary" sx={{
-            backgroundColor: "white", 
+            backgroundColor: displayInput ? "white" : "transparent",
             borderRadius: "28px",
             display: "flex",
             alignItems: "center",
             gap: "0.5rem",
             paddingLeft: "1rem",
             }}>
-        <SearchIcon fontSize="medium" color="primary"/>
+        <SearchIcon fontSize="medium" color="primary" 
+            onClick={()=>setDisplayInput(!displayInput)}/>
         <TextField variant="standard"  
-            sx={{ marginRight: "1rem" }}
+            sx={{ 
+                marginRight: "1rem",
+                display: displayInput ? "block" : "none"
+            }}
             InputProps={{
                 disableUnderline: true,
             }}
         />
-        </Box>
+        </Box> 
+        
+
         </>
     );
 }
