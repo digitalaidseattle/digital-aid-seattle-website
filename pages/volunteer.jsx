1080fce092706693dcb4aa877a4fece1290fdc87
Misty Becker <mistyb01@uw.edu>
2023-06-15 18:04:10 -0700

    removed test code

--- pages/volunteer.jsx
+++ pages/volunteer.jsx
@@ -1,19 +1,10 @@
 import Container from '../components/Container'
 import Layout from '../components/Layout'
-import CardEvent from '../components/cards/CardEvent'
 
 export default function VolunteerForm() {
   return (
     <Layout>
       <Container>
-        <CardEvent
-          title="Earth-a-thon"
-          date="March 27, 2023"
-          time={{start: "6:00", end: "8:00 PM"}}
-          location="123 ABC Street, Seattle, WA"
-          description="Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-          buttonLink="#"
-        />
         <iframe
           className="airtable-embed airtable-dynamic-height"
           src="https://airtable.com/embed/shr1lbcr3qmkoIbNW?backgroundColor=purpleLight"
