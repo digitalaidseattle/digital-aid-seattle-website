0550adfca0e504a8cc7052af07423af075f635a4
Misty Becker <mistyb01@uw.edu>
2023-06-19 10:46:42 -0700

    ran prettier

--- components/filters/FilterContainer.tsx
+++ components/filters/FilterContainer.tsx
@@ -1,39 +1,36 @@
-import { ReactNode } from 'react'
-import Stack from '@mui/material/Stack';
-import { SelectChangeEvent } from "@mui/material/Select";
-import { useState } from "react";
+import Stack from '@mui/material/Stack'
+import { SelectChangeEvent } from '@mui/material/Select'
+import { useState } from 'react'
 import FilterDropdown from './FilterDropdown'
 
-
 const FilterContainer = () => {
-  const [roleSelection, setRoleSelection] = useState(undefined);
-  const [programSelection, setProgramSelection] = useState(undefined);
+  const [roleSelection, setRoleSelection] = useState(undefined)
+  const [programSelection, setProgramSelection] = useState(undefined)
 
   const handleRoleChange = (event: SelectChangeEvent) => {
-      setRoleSelection(event.target.value as string);
-  };
+    setRoleSelection(event.target.value as string)
+  }
 
   const handleProgramChange = (event: SelectChangeEvent) => {
-      setProgramSelection(event.target.value as string);
-  };
-  
-    return (
-      <Stack direction="row" spacing={{xs: "1rem", md: "2rem"}}>
-            <FilterDropdown 
-              name="Role Needed" 
-              options={["Design", "Engineering", "Project Management"]}
-              currentValue={roleSelection} 
-              handleChange={handleRoleChange} 
-            />
-            <FilterDropdown 
-              name="Program Area" 
-              options={["Civic equity", "Environment", "Animals", "LGBTQIA+"]}
-              currentValue={programSelection} 
-              handleChange={handleProgramChange} 
-            />
-      </Stack>
-    );
+    setProgramSelection(event.target.value as string)
+  }
+
+  return (
+    <Stack direction="row" spacing={{ xs: '1rem', md: '2rem' }}>
+      <FilterDropdown
+        name="Role Needed"
+        options={['Design', 'Engineering', 'Project Management']}
+        currentValue={roleSelection}
+        handleChange={handleRoleChange}
+      />
+      <FilterDropdown
+        name="Program Area"
+        options={['Civic equity', 'Environment', 'Animals', 'LGBTQIA+']}
+        currentValue={programSelection}
+        handleChange={handleProgramChange}
+      />
+    </Stack>
+  )
 }
 
-
-export default FilterContainer;
+export default FilterContainer
--- components/filters/SearchContainer.tsx
+++ components/filters/SearchContainer.tsx
@@ -1,25 +1,25 @@
 import { ReactNode } from 'react'
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box'
 
 type Props = {
-    children: ReactNode
-  }
-
-const SearchContainer = ({ children }: Props) => {
-  
-    return (
-      <Box sx={{ 
-        backgroundColor: '#F7FAFA', 
-        padding: '2rem', 
-        display: 'flex',
-        flexDirection: {xs: 'column', md: 'row'}, 
-        justifyContent: 'center',
-        gap: '2rem',
-}}>
-        {children}
-      </Box>
-    );
+  children: ReactNode
 }
 
+const SearchContainer = ({ children }: Props) => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: '#F7FAFA',
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' },
+        justifyContent: 'center',
+        gap: '2rem',
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
 
-export default SearchContainer;
+export default SearchContainer
