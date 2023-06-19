ec983f284d8605a664f791b42f94ef45c5cb003d
Santina Lin <santina.lin@convoy.com>
2023-06-19 09:57:09 -0700

    add all the links

--- components/CommonFooter.tsx
+++ components/CommonFooter.tsx
@@ -4,6 +4,7 @@
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import { Container, Grid, Stack, styled, Typography } from '@mui/material'
+import Link from '@mui/material/Link'
 import { theme } from 'theme/theme'
 
 import OSLogo from '../assets/darkThemeLogo.svg'
@@ -30,6 +31,18 @@ const FooterSubItem = styled(Typography)(({ theme }) => ({
   color: theme.palette.primary.contrastText,
 }))
 
+const LinkSubItem = (props: { url: string; name: string }) => (
+  <Link
+    sx={{
+      color: theme.palette.primary.contrastText,
+    }}
+    underline="hover"
+    href={props.url}
+  >
+    <Typography>{props.name}</Typography>
+  </Link>
+)
+
 const SubText = styled(Typography)(({ theme }) => ({
   color: theme.palette.primary.contrastText,
   fontSize: '0.75rem',
@@ -46,6 +59,7 @@ const CommonFooter = () => {
             style={{
               height: '80%',
               width: '120px',
+              margin: '0rem 0rem 1rem',
             }}
           />
           <SubText>Illustration by: Kylie Melendez</SubText>
@@ -54,24 +68,40 @@ const CommonFooter = () => {
 
         <GridItem xs={4}>
           <FooterItemTitle>Contact us</FooterItemTitle>
-          <FooterSubItem>info@openseattle.org</FooterSubItem>
+          <a href="mailto:name@email.com">
+            <FooterSubItem>info@openseattle.org</FooterSubItem>
+          </a>
         </GridItem>
         <GridItem xs={4}>
           <Stack direction="column" spacing={2}>
             <Item>
               <FooterItemTitle>Work with us</FooterItemTitle>
-              <FooterSubItem>Volunteer</FooterSubItem>
-              <FooterSubItem>Partner</FooterSubItem>
+              <LinkSubItem url="/volunteers_new" name="Volunteers" />
+              <LinkSubItem url="/partners_new" name="Partners" />
             </Item>
             <Item>
               <FooterItemTitle>About</FooterItemTitle>
-              <FooterSubItem>Privacy Policy</FooterSubItem>
+              <LinkSubItem url="/privacy-policy" name="Privacy Policy" />
             </Item>
           </Stack>
         </GridItem>
         <GridItem xs={1}>
-          <GitHubIcon sx={{ color: theme.palette.primary.contrastText }} />
-          <LinkedInIcon sx={{ color: theme.palette.primary.contrastText }} />
+          <Stack direction="column" spacing={2}>
+            <Item>
+              <a href="https://github.com/openseattle">
+                <GitHubIcon
+                  sx={{ color: theme.palette.primary.contrastText }}
+                />
+              </a>
+            </Item>
+            <Item>
+              <a href="https://www.linkedin.com/company/open-seattle/">
+                <LinkedInIcon
+                  sx={{ color: theme.palette.primary.contrastText }}
+                />
+              </a>
+            </Item>
+          </Stack>
         </GridItem>
       </Grid>
     </Container>
