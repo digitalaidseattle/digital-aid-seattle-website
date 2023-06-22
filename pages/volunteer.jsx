550d66a8b178a65ab94beea1c4944ff651f339a1
Misty Becker <mistyb01@uw.edu>
2023-06-21 19:33:17 -0700

    remove test code

--- pages/volunteer.jsx
+++ pages/volunteer.jsx
@@ -1,63 +1,12 @@
 import Container from '../components/Container'
 import Layout from '../components/Layout'
 
-import CardProject from '../components/cards/CardProject'
-import CardGridContainer from '../components/cards/CardGridContainer'
+
 
 export default function VolunteerForm() {
   return (
     <Layout>
       <Container>
-      <CardGridContainer columns={4}>
-        <CardProject
-          title= "Clearviction"
-          partner="City of Bellevue"
-          programAreas={["Civic engagement"]}
-          description= "Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-          status= "active"
-          projectLink= "#"
-          duration= {{start: "Jun 1", end: "Jul 15"}}/>
-        <CardProject
-          title= "Seattle Humane Society"
-          partner="City of Seattle"
-          programAreas={["Animals"]}
-          description= "Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-          status= "complete"
-          projectLink= "#"
-          duration= {{start: "Mar 1", end: "Mar 15"}}/>        
-        <CardProject
-          title= "Open Seattle Cadre"
-          partner="Open Seattle"
-          programAreas={["Civic engagement", "Technology"]}
-          description= "Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-          status= "recruiting"
-          projectLink= "#"
-          duration= {{start: "May 1", end: "May 15"}}/>
-                  <CardProject
-          title= "Open Seattle Cadre"
-          partner="Open Seattle"
-          programAreas={["Civic engagement", "Technology"]}
-          description= "Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-          status= "recruiting"
-          projectLink= "#"
-          duration= {{start: "May 1", end: "May 15"}}/>
-                  <CardProject
-          title= "Open Seattle Cadre"
-          partner="Open Seattle"
-          programAreas={["Civic engagement", "Technology"]}
-          description= "Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-          status= "recruiting"
-          projectLink= "#"
-          duration= {{start: "May 1", end: "May 15"}}/>
-                  <CardProject
-          title= "Open Seattle Cadre"
-          partner="Open Seattle"
-          programAreas={["Civic engagement", "Technology"]}
-          description= "Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
-          status= "recruiting"
-          projectLink= "#"
-          duration= {{start: "May 1", end: "May 15"}}/>
-      </CardGridContainer>
         <iframe
           className="airtable-embed airtable-dynamic-height"
           src="https://airtable.com/embed/shr1lbcr3qmkoIbNW?backgroundColor=purpleLight"
