a21ee07bf9cf0f9ab1b3d7b565c831bb8774d22e
Misty Becker <mistyb01@uw.edu>
2023-07-06 13:43:06 -0700

    ran prettier

--- pages/projects_new.tsx
+++ pages/projects_new.tsx
@@ -95,15 +95,17 @@ const ProjectsPage = () => {
         )}
       </Stack>
 
-    <Stack 
-    sx={{
-      width: '100%',
-      backgroundColor: theme.palette.primary.contrastText,
-      padding: {
-        xs: '2.5rem 1rem 4rem 1rem', md: '2.5rem 2rem 4rem 2rem', lg: '2.5rem 12.5rem 7.5rem 12.5rem' 
-      }
-      }}>
-
+      <Stack
+        sx={{
+          width: '100%',
+          backgroundColor: theme.palette.primary.contrastText,
+          padding: {
+            xs: '2.5rem 1rem 4rem 1rem',
+            md: '2.5rem 2rem 4rem 2rem',
+            lg: '2.5rem 12.5rem 7.5rem 12.5rem',
+          },
+        }}
+      >
         <CardGridContainer>
           {projectData.map((project) => (
             <CardProject
