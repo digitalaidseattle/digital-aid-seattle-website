eecaf5d7e2bf8ffbfbdb54caca6c9b77584b9617
mlndlm <131744661+mlndlm@users.noreply.github.com>
2023-07-07 09:15:32 -0700

    Create ListItemWithImage component (#25)
    * Create ListItemWithImage component
    
    * use useTheme instead of designColor for styling

---
+++ components/cards/ListItemWithImage.tsx
@@ -0,0 +1,46 @@
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import { useTheme } from '@mui/material'
+
+type ListItemWithImageProps = {
+  listText: string
+  listImage: string
+  listImageAltText?: string
+}
+
+const ListItemWithImage = ({
+  listText,
+  listImage,
+  listImageAltText = 'decorative',
+}: ListItemWithImageProps) => {
+  const theme = useTheme()
+  const palette = theme.palette
+
+  return (
+    <ListItem
+      sx={{
+        boxShadow:
+          '0px 4px 8px 2px rgba(52, 61, 62, 0.04), 0px 2px 4px rgba(52, 61, 62, 0.04)',
+      }}
+    >
+      <ListItemAvatar sx={{ color: palette.primary.light }}>
+        <Avatar alt={listImageAltText} src={listImage} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={
+          <Typography
+            variant="labelMedium"
+            color={palette.secondary.contrastText}
+          >
+            {listText}
+          </Typography>
+        }
+      />
+    </ListItem>
+  )
+}
+
+export default ListItemWithImage
