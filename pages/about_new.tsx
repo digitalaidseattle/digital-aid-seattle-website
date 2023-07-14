c5bb0bbeb600d428ba8b9368c75c43de8a1bd410
Santina Lin <santina.lin@convoy.com>
2023-07-13 19:57:23 -0700

    refactor about us

--- pages/about_new.tsx
+++ pages/about_new.tsx
@@ -17,6 +17,8 @@ import CardRowContainer from 'components/cards/CardRowContainer'
 import { withBasicLayout } from 'components/layouts'
 import { designColor } from 'theme/theme'
 
+import AboutUsImage from '../assets/aboutUs.png'
+
 // TODO - use theme instead of designColor
 const SectionContainer = styled(Container)(({ theme }) => ({
   backgroundColor: designColor.white,
@@ -27,139 +29,137 @@ const SectionContainer = styled(Container)(({ theme }) => ({
 
 const AboutPage = () => {
   const theme = useTheme()
-  const palette = theme.palette
 
   return (
     <div>
-      <Container
-        sx={{
-          backgroundColor: palette.primary.light,
-          padding: { md: '2rem' },
-        }}
-        maxWidth={false}
-      >
-        <Grid container spacing={20}>
-          <Grid item xs={6}>
-            <Container sx={{ margin: '10rem' }}>
-              <div>
-                <Typography
-                  variant="displayMedium"
-                  sx={{ color: theme.palette.primary.contrastText }}
-                >
-                  About us
-                </Typography>
-              </div>
-              <div>
-                <Typography
-                  variant="bodyLarge"
-                  sx={{ color: theme.palette.primary.contrastText }}
-                >
-                  Open Seattle comprises a group of seasoned professionals with
-                  experience in web development, software engineering, digital
-                  strategy, visual and content design, and more. We leverage
-                  this vast experience to transform the nonprofit sector—to make
-                  it more efficient, effective, and accessible to all.
-                </Typography>
-              </div>
-            </Container>
-          </Grid>
-          <Grid item xs={6}>
-            <img
-              className="w-200 md:w-500 lg:w-700 object-contain"
-              src="rafiki.png"
-              alt=""
-            />
-          </Grid>
-        </Grid>
-      </Container>
-
-      <SectionContainer
-        sx={{
-          backgroundColor: designColor.white,
-        }}
-        maxWidth={false}
-      >
-        <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
-          <Typography variant="headlineMedium">What we do</Typography>
-          <Typography variant="bodyLarge">
-            Open Seattle partners with other nonprofits in order to amplifying
-            their impact, and to uplift communities with technology.
-          </Typography>
-        </Box>
-      </SectionContainer>
-
-      <SectionContainer
-        style={{
-          backgroundColor: theme.palette.background.default,
-        }}
-        maxWidth={false}
-      >
-       <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
-          <Typography variant="headlineMedium">Our values</Typography>
-       
-        <Grid container spacing={2}>
-          <Grid item md={4}>
-            <CardOne
-              title="Excellence"
-              description="Striving for professional excellence means taking an uncompromising approach to the service we endeavor to provide. We ensure the utmost quality in what we deliver."
-              icon={
-                <MilitaryTechOutlined style={{ color: designColor.white }} />
-              }
-            />
-          </Grid>
-          <Grid item md={4}>
-            <CardOne
-              title="Efficacy"
-              description="What we do will have impact. We will apply the pareto principle (and other frameworks) to ensure that we are optimizing our efforts at every step in our process from engagement to delivery."
-              icon={<AutoGraphOutlined style={{ color: designColor.white }} />}
-            />
-          </Grid>
-          <Grid item md={4}>
-            <CardOne
-              title="Efficiency"
-              description="We work with an eye toward maintaining a steady and speedy cadence whenever possible. We don't sacrifice quality, but we work with an MLP (minimum loveable product) mindset."
-              icon={
-                <AccessAlarmOutlined style={{ color: designColor.white }} />
-              }
-            />
-          </Grid>
-        </Grid>
-        </Box>
-      </SectionContainer>
-
-      <SectionContainer
-        style={{
-          backgroundColor: designColor.white,
-        }}
-        maxWidth={false}
-      >
-        <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
-          <Typography variant="headlineMedium">Our vision</Typography>
-          <Typography variant="bodyLarge" align="center" display="block">
-            We envision a network of highly functional non-profits in the Puget
-            Sound area, working in tandem with one another and with the
-            municipal government in order to enable the highest echelon of
-            public service.
-          </Typography>
-        </Box>
-      </SectionContainer>
-
-      <SectionContainer
-        style={{
-          backgroundColor: theme.palette.background.default,
-        }}
-        maxWidth={false}
-      >
-       <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
-          <Typography variant="headlineMedium">Our team</Typography>
-          <Typography variant="bodyLarge" align="center" display="block">
-            The Open Seattle cadre is made up of highly skilled and committed
-            volunteers, dedicated to serving the greater Seattle area.
-          </Typography>
-        </Box>
-      </SectionContainer>
+      
     </div>
   )
 }
 
 export default withBasicLayout(AboutPage)
+
+const AboutUsHeroSection = ({ theme }) => (
+  <Container
+    sx={{
+      backgroundColor: theme.palette.primary.main,
+      padding: '5rem',
+    }}
+    maxWidth="xl"
+  >
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid item md={4} xs={6}>
+        <>
+          <Typography
+            variant="displayLarge"
+            sx={{ color: theme.palette.primary.contrastText }}
+          >
+            About us
+          </Typography>
+          <Typography
+            variant="bodyLarge"
+            sx={{ color: theme.palette.primary.contrastText }}
+          >
+            Open Seattle comprises a group of seasoned professionals with
+            experience in web development, software engineering, digital
+            strategy, visual and content design, and more. We leverage this vast
+            experience to transform the nonprofit sector—to make it more
+            efficient, effective, and accessible to all.
+          </Typography>
+        </>
+      </Grid>
+      <Grid item md={4} xs={6}>
+        <img src={AboutUsImage.src} alt="About Us page graphic"></img>
+      </Grid>
+    </Grid>
+  </Container>
+)
+
+const WhatWeDoSection = ({ theme }) => {
+  ;<SectionContainer
+    sx={{
+      backgroundColor: designColor.white,
+    }}
+    maxWidth={false}
+  >
+    <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
+      <Typography variant="headlineMedium">What we do</Typography>
+      <Typography variant="bodyLarge">
+        Open Seattle partners with other nonprofits in order to amplifying their
+        impact, and to uplift communities with technology.
+      </Typography>
+    </Box>
+  </SectionContainer>
+}
+
+const OurValueSection = ({ theme }) => (
+  <SectionContainer
+    style={{
+      backgroundColor: theme.palette.background.default,
+    }}
+    maxWidth={false}
+  >
+    <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
+      <Typography variant="headlineMedium">Our values</Typography>
+
+      <Grid container spacing={2}>
+        <Grid item md={4}>
+          <CardOne
+            title="Excellence"
+            description="Striving for professional excellence means taking an uncompromising approach to the service we endeavor to provide. We ensure the utmost quality in what we deliver."
+            icon={<MilitaryTechOutlined style={{ color: designColor.white }} />}
+          />
+        </Grid>
+        <Grid item md={4}>
+          <CardOne
+            title="Efficacy"
+            description="What we do will have impact. We will apply the pareto principle (and other frameworks) to ensure that we are optimizing our efforts at every step in our process from engagement to delivery."
+            icon={<AutoGraphOutlined style={{ color: designColor.white }} />}
+          />
+        </Grid>
+        <Grid item md={4}>
+          <CardOne
+            title="Efficiency"
+            description="We work with an eye toward maintaining a steady and speedy cadence whenever possible. We don't sacrifice quality, but we work with an MLP (minimum loveable product) mindset."
+            icon={<AccessAlarmOutlined style={{ color: designColor.white }} />}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  </SectionContainer>
+)
+
+const OurVisionSection = ({ theme }) => (
+  <SectionContainer
+    style={{
+      backgroundColor: designColor.white,
+    }}
+    maxWidth={false}
+  >
+    <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
+      <Typography variant="headlineMedium">Our vision</Typography>
+      <Typography variant="bodyLarge" align="center" display="block">
+        We envision a network of highly functional non-profits in the Puget
+        Sound area, working in tandem with one another and with the municipal
+        government in order to enable the highest echelon of public service.
+      </Typography>
+    </Box>
+  </SectionContainer>
+)
+
+const OurTeamSection = ({ theme }) => (
+  <SectionContainer
+    style={{
+      backgroundColor: theme.palette.background.default,
+    }}
+    maxWidth={false}
+  >
+    <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
+      <Typography variant="headlineMedium">Our team</Typography>
+      <Typography variant="bodyLarge" align="center" display="block">
+        The Open Seattle cadre is made up of highly skilled and committed
+        volunteers, dedicated to serving the greater Seattle area.
+      </Typography>
+    </Box>
+  </SectionContainer>
+)
