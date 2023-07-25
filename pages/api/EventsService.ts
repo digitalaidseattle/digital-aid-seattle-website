9558d223e1344b4952e5d4ceb9dc138e17858be8
Jeffrey Nakasone <jnakaso@yahoo.com>
2023-07-25 09:10:54 -0700

    Externalized events; fix background color (#38)

---
+++ pages/api/EventsService.ts
@@ -0,0 +1,56 @@
+/*
+* @2023 Open Seattle
+*/
+
+import EventPlaceholder from '../../assets/event-placeholder.png';
+import localEvents from '../../assets/events.json';
+
+class OsEventTime {
+    start: string;
+    end: string;
+
+    constructor(data: any) {
+        this.start = data.start;
+        this.end = data.end;
+    }
+}
+
+class OSEvent {
+    title: string;
+    date: string;
+    time: OsEventTime;
+    location: string;
+    description: string;
+    imageSrc: string;
+    imageAlt: string;
+    buttonLink: string;
+    active = true;
+
+    constructor(data: any) {
+        this.title = data.title;
+        this.date = data.date;
+        this.time = new OsEventTime({ start: data.start, end: data.end });
+        this.location = data.location;
+        this.description = data.description ? data.description : '';
+        this.imageSrc = data.imageSrc ? data.imageSrc : EventPlaceholder.src;
+        this.imageAlt = data.imageAlt;
+        this.buttonLink = data.buttonLink;
+    }
+}
+
+class OSEventService {
+
+    getActiveEvents(): OSEvent[] {
+        // TODO make environment dependent use REST or resource file
+        return this.getResourceFile();
+    }
+
+    getResourceFile(): OSEvent[] {
+        return localEvents.map(json => new OSEvent(json));
+    }
+
+}
+
+const osEventsService = new OSEventService();
+
+export { OSEvent, OsEventTime, osEventsService };
