4f5494c7127a1db2cdbe6366cd8871180c31cf9a
Santina Lin <santina.lin@convoy.com>
2023-07-31 21:44:50 -0700

    fix spacing for other sections

--- pages/about_new.tsx
+++ pages/about_new.tsx
@@ -92,77 +92,87 @@ const AboutUsHeroSection = ({ theme, extraSmallScreen }) => (
 )
 
 const WhatWeDoSection = ({ theme, extraSmallScreen }) => (
-  <SectionContainer
-    backgroundColor={designColor.white}
-  >
-    <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
+  <SectionContainer backgroundColor={designColor.white}>
+    <Stack
+      gap={{ xs: '64px', md: '80px' }}
+      sx={{
+        textAlign: 'center',
+      }}
+      maxWidth={'880px'}
+    >
       <Typography variant="headlineMedium">What we do</Typography>
       <Typography variant="bodyLarge">
         Open Seattle partners with other nonprofits in order to amplifying their
         impact, and to uplift communities with technology.
       </Typography>
-    </Box>
+    </Stack>
   </SectionContainer>
 )
 
 const OurValueSection = ({ theme, extraSmallScreen }) => (
-  <SectionContainer
-  backgroundColor={theme.palette.background.default}
-  >
+  <SectionContainer backgroundColor={theme.palette.background.default}>
     <Stack
-        gap={{ xs: '64px', md: '80px' }}
-        sx={{
-          textAlign: 'center',
-        }}
-        maxWidth={'880px'}
-      >
-    <Typography variant="headlineMedium">Our values</Typography>
-    <CardRowContainer>
-      <CardOne
-        title="Excellence"
-        description="Striving for professional excellence means taking an uncompromising approach to the service we endeavor to provide. We ensure the utmost quality in what we deliver."
-        icon={<MilitaryTechOutlined style={{ color: designColor.white }} />}
-      />
-      <CardOne
-        title="Efficacy"
-        description="What we do have impact. We apply the pareto principle (and other frameworks) to ensure that we  optimize our efforts from engagement to delivery."
-        icon={<AutoGraphOutlined style={{ color: designColor.white }} />}
-      />
-      <CardOne
-        title="Efficiency"
-        description="We work with a steady and speedy cadence whenever possible. We maintain a MLP (minimum loveable product) mindset without sacrificing the quality of our work."
-        icon={<AccessAlarmOutlined style={{ color: designColor.white }} />}
-      />
-    </CardRowContainer>
+      gap={{ xs: '64px', md: '80px' }}
+      sx={{
+        textAlign: 'center',
+      }}
+      maxWidth={'880px'}
+    >
+      <Typography variant="headlineMedium">Our values</Typography>
+      <CardRowContainer>
+        <CardOne
+          title="Excellence"
+          description="Striving for professional excellence means taking an uncompromising approach to the service we endeavor to provide. We ensure the utmost quality in what we deliver."
+          icon={<MilitaryTechOutlined style={{ color: designColor.white }} />}
+        />
+        <CardOne
+          title="Efficacy"
+          description="What we do have impact. We apply the pareto principle (and other frameworks) to ensure that we  optimize our efforts from engagement to delivery."
+          icon={<AutoGraphOutlined style={{ color: designColor.white }} />}
+        />
+        <CardOne
+          title="Efficiency"
+          description="We work with a steady and speedy cadence whenever possible. We maintain a MLP (minimum loveable product) mindset without sacrificing the quality of our work."
+          icon={<AccessAlarmOutlined style={{ color: designColor.white }} />}
+        />
+      </CardRowContainer>
     </Stack>
   </SectionContainer>
 )
 
 const OurVisionSection = ({ theme, extraSmallScreen }) => (
-  <SectionContainer
-  backgroundColor={designColor.white}
-  >
-    <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
+  <SectionContainer backgroundColor={designColor.white}>
+    <Stack
+      gap={{ xs: '64px', md: '80px' }}
+      sx={{
+        textAlign: 'center',
+      }}
+      maxWidth={'880px'}
+    >
       <Typography variant="headlineMedium">Our vision</Typography>
       <Typography variant="bodyLarge" align="center" display="block">
         We envision a network of highly functional non-profits in the Puget
         Sound area, working in tandem with one another and with the municipal
         government in order to enable the highest echelon of public service.
       </Typography>
-    </Box>
+    </Stack>
   </SectionContainer>
 )
 
 const OurTeamSection = ({ theme, extraSmallScreen }) => (
-  <SectionContainer
-  backgroundColor={theme.palette.background.default}
-  >
-    <Box sx={{ gap: '2rem', display: 'grid', margin: '2rem' }}>
+  <SectionContainer backgroundColor={theme.palette.background.default}>
+    <Stack
+      gap={{ xs: '64px', md: '80px' }}
+      sx={{
+        textAlign: 'center',
+      }}
+      maxWidth={'880px'}
+    >
       <Typography variant="headlineMedium">Our team</Typography>
       <Typography variant="bodyLarge" align="center" display="block">
         The Open Seattle cadre is made up of highly skilled and committed
         volunteers, dedicated to serving the greater Seattle area.
       </Typography>
-    </Box>
+    </Stack>
   </SectionContainer>
 )
