c7e50d5e7e8f0eff465fd3f407dffa260c076cec
Santina Lin <santina.lin@convoy.com>
2023-07-31 22:36:49 -0700

    fix list item height and line height

--- components/list/ListItemWithIcon.tsx
+++ components/list/ListItemWithIcon.tsx
@@ -3,7 +3,7 @@ import ListItem from '@mui/material/ListItem'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import Typography from '@mui/material/Typography'
-import { useTheme } from '@mui/material'
+import { Box, useTheme } from '@mui/material'
 
 type ListItemWithIconProps = {
   listIcon: ReactNode
@@ -19,7 +19,9 @@ const ListItemWithIcon = ({ listIcon, listText }: ListItemWithIconProps) => {
       sx={{
         boxShadow:
           '0px 4px 8px 2px rgba(52, 61, 62, 0.04), 0px 2px 4px rgba(52, 61, 62, 0.04)',
+        height: '56px'
       }}
+      dense={true}
     >
       <ListItemIcon sx={{ color: palette.primary.light }}>
         {listIcon}
@@ -30,7 +32,7 @@ const ListItemWithIcon = ({ listIcon, listText }: ListItemWithIconProps) => {
             variant="labelMedium"
             color={palette.secondary.contrastText}
           >
-            {listText}
+            <Box sx={{ lineHeight: 1.5 }}>{listText}</Box>
           </Typography>
         }
       />
