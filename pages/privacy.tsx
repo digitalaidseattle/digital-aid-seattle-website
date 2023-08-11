e909bba92f62d7858148f4611ab1be66937affaa
jnakaso <jnakaso@yahoo.com>
2023-08-11 07:03:18 -0700

    remove lint warnings

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -97,13 +97,13 @@ const PrivacyPage = () => {
 
                     <SectionHead title="Log Data" />
                     <SectionBody>
-                        <p>We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer’s Internet Protocol ("IP") address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.</p>
+                        <p>{`We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer’s Internet Protocol ("IP") address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.`}</p>
                     </SectionBody>
 
                     <SectionHead title="Cookies" />
                     <SectionBody>
                         <p>Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer’s hard drive.</p>
-                        <p>Our website uses these "cookies" to collection information and to improve our Service. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our Service.</p>
+                        <p>{`Our website uses these "cookies" to collection information and to improve our Service. You have the option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you choose to refuse our cookies, you may not be able to use some portions of our Service.`}</p>
                     </SectionBody>
 
                     <SectionHead title="Service Providers" />
