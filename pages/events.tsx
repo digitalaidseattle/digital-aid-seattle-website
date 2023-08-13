fed49cd32c6268b3f778db43b33d58c4aaa461ca
Jeffrey Nakasone <jnakaso@yahoo.com>
2023-08-12 19:19:14 -0700

    privacy policy page (#49)
    * first draft
    
    * trying page name
    
    * remove lint warnings
    
    * Incorporate styling updates
    
    * removed extra stack

--- pages/events.tsx
+++ pages/events.tsx
@@ -4,51 +4,15 @@
 
 import {
   Container,
-  Typography,
-  useTheme,
+  useTheme
 } from '@mui/material'
+import Masthead from 'components/Masthead'
 import CardEvent from 'components/cards/CardEvent'
 import CardRowContainer from 'components/cards/CardRowContainer'
 import { withBasicLayout } from 'components/layouts'
 import { useEffect, useState } from 'react'
-
 import { osEventsService } from './api/EventsService'
 
-type MastheadProps = {
-  title: string
-}
-
-// REVIEW: Consider promoting
-
-const Masthead = ({ title }: MastheadProps) => {
-  const theme = useTheme()
-
-  return (
-    <Container
-      sx={{
-        backgroundColor: theme.palette.primary.main,
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        paddingY: '5rem'
-      }}
-      maxWidth={false}
-    >
-      <Typography
-        variant="displayMedium"
-        sx={{
-          color: theme.palette.primary.contrastText,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        {title}
-      </Typography>
-    </Container>
-  )
-
-}
 
 const EventsPage = () => {
   const theme = useTheme()
@@ -63,7 +27,7 @@ const EventsPage = () => {
 
   return (
     <div>
-      <Masthead title="Events" />
+      <Masthead title={title} />
       <Container
         sx={{
           backgroundColor: palette.primary.contrastText,
