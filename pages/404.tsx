7cfc480ceb7008a0adebf5faf5a1ac7bb2a20e6b
jnakaso <jnakaso@yahoo.com>
2023-08-15 20:59:34 -0700

    404 page design & responsive udpates

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,64 +1,80 @@
 /*
 * @2023 Open Seattle
 */
-import { Box, Container, Link, Typography, useTheme } from '@mui/material'
+import { Box, Button, Container, Typography, useMediaQuery, useTheme } from '@mui/material'
 import SectionContainer from 'components/layout/SectionContainer'
 import { withBasicLayout } from 'components/layouts'
 import ErrorImage from '../assets/404-error.png'
 
-function Page404() {
-  const theme = useTheme()
-  const palette = theme.palette
+// Promote, if we want to handle other error types
+type ErrorComponentProps = {
+  title: string,
+  description: string,
+  imageSrc: string,
+  imageTitle: string,
+}
+const ErrorComponent = ({ title, description, imageSrc, imageTitle }: ErrorComponentProps) => {
 
-  return (
-    <>
-      <Container
-        maxWidth={false}
-        disableGutters
-        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-      >
-        <SectionContainer backgroundColor={theme.palette.background}>
-          <Container
-            sx={{
-              display: 'flex',
-              flexDirection: { xs: 'column', lg: 'row' },
-              gap: { xs: 4, md: '40px' },
-            }}
+  const theme = useTheme()
+  const smallScreen = useMediaQuery(theme.breakpoints.only('xs'))
+  const largeScreen = useMediaQuery(theme.breakpoints.up('lg'))
+
+  return <SectionContainer backgroundColor={theme.palette.background}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', lg: 'row' },
+        alignItems: 'center',
+        gap: { xs: 4, md: '40px' },
+      }}>
+      <Box
+        sx={{
+          textAlign: 'left',
+          width: '100%',
+          maxWidth: largeScreen ? '418px' : '950px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '2rem',
+        }}>
+        <Typography
+          variant={smallScreen ? 'displayMedium' : 'displayLarge'}>
+          {title}
+        </Typography>
+        <Typography variant="bodyLarge" >
+          {description}
+        </Typography>
+        <Box >
+          <Button
+            title="Back Home"
+            variant="contained"
+            color="primary"
+            href="/"
           >
-            <Box
-              sx={{
-                textAlign: 'left',
-                width: '100%',
-                maxWidth: '418px',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '2rem',
-              }}
-            >
-              <img
-                src={ErrorImage.src}
-                title="Error - 404"
-                alt="404 Error graphic"
-                width="418px"
-                style={{ objectFit: 'cover' }}
-              />
-            </Box>
-            <Container id="aasd" sx={{textAlign: 'center'}}>
-              <Typography variant="bodyLarge" >
-                {"The page you're trying to access couldn't be located. Please click here to return to our "}
-                <Link
-                  href="/"
-                  className="font-body leading-6 "
-                >
-                  homepage
-                </Link>
-                .
-              </Typography>
-            </Container>
-          </Container>
-        </SectionContainer>
-      </Container>
-    </ >
+            Back Home
+          </Button>
+        </Box>
+      </Box>
+      <img
+        src={imageSrc}
+        alt={imageTitle}
+        width="418px"
+        style={{ objectFit: 'cover' }}
+      />
+    </Box>
+  </SectionContainer>
+}
+
+function Page404() {
+  return (
+    <Container
+      maxWidth={false}
+      disableGutters
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <ErrorComponent title="Page Not Found"
+        description="Unfortunately, the page you are trying to access could not be located. Please return to our home page to continue your journey."
+        imageSrc={ErrorImage.src}
+        imageTitle="404 Error Image" />
+    </Container>
   )
 }
 
