599e007568db5f3f3d0d5ddf3c769eef06754dc3
Yulia Balenko <yulia@YuliaBookPro13.local>
2023-08-18 18:10:51 -0700

    Fixed word order in How you can help

--- components/CommonFooter.tsx
+++ components/CommonFooter.tsx
@@ -10,302 +10,302 @@ import { theme } from 'theme/theme'
 import OSLogo from '../assets/darkThemeLogo.svg'
 
 const GridItem = styled(Grid)(() => ({
-  item: true,
-  padding: '0.5rem',
-  justifyContent: 'left',
-  alignItems: 'center',
+    item: true,
+    padding: '0.5rem',
+    justifyContent: 'left',
+    alignItems: 'center',
 }))
 
 const FooterItemTitle = styled(Typography)(({ theme }) => ({
-  fontWeight: 700,
-  color: theme.palette.primary.contrastText,
-  fontSize: '0.875rem',
+    fontWeight: 700,
+    color: theme.palette.primary.contrastText,
+    fontSize: '0.875rem',
 }))
 
 const FooterSubItem = styled(Typography)(({ theme }) => ({
-  color: theme.palette.primary.contrastText,
-  fontSize: '1rem',
-  marginTop: '1rem',
-  display: 'block',
+    color: theme.palette.primary.contrastText,
+    fontSize: '1rem',
+    marginTop: '1rem',
+    display: 'block',
 }))
 
 const LinkSubItem = (props: { url: string; name: string }) => (
-  <Link
-    sx={{
-      color: theme.palette.primary.contrastText,
-      marginTop: '1rem',
-      display: 'block',
-    }}
-    underline="hover"
-    href={props.url}
-  >
-    <Typography fontSize={'1rem'}>{props.name}</Typography>
-  </Link>
+    <Link
+        sx={{
+            color: theme.palette.primary.contrastText,
+            marginTop: '1rem',
+            display: 'block',
+        }}
+        underline="hover"
+        href={props.url}
+    >
+        <Typography fontSize={'1rem'}>{props.name}</Typography>
+    </Link>
 )
 
 const SubText = styled(Typography)(({ theme }) => ({
-  color: theme.palette.primary.contrastText,
-  fontSize: '0.75rem',
+    color: theme.palette.primary.contrastText,
+    fontSize: '0.75rem',
 }))
 
 const CommonFooterLargeScreen = () => (
-  <Grid container direction="row" rowGap="2.5rem">
-    {/* LOGO */}
-    <GridItem md={3}>
-      <img
-        src={OSLogo.src}
-        style={{
-          height: '80%',
-          width: '120px',
-        }}
-      />
-    </GridItem>
+    <Grid container direction="row" rowGap="2.5rem">
+        {/* LOGO */}
+        <GridItem md={3}>
+            <img
+                src={OSLogo.src}
+                style={{
+                    height: '80%',
+                    width: '120px',
+                }}
+            />
+        </GridItem>
 
-    <GridItem md={4}>
-      <FooterItemTitle>Contact us</FooterItemTitle>
-      <FooterSubItem>
-        <a href="mailto:name@email.com">info@openseattle.org</a>
-      </FooterSubItem>
-    </GridItem>
+        <GridItem md={4}>
+            <FooterItemTitle>Contact us</FooterItemTitle>
+            <FooterSubItem>
+                <a href="mailto:name@email.com">info@openseattle.org</a>
+            </FooterSubItem>
+        </GridItem>
 
-    <GridItem md={4}>
-      <FooterItemTitle>Work with us</FooterItemTitle>
-      <LinkSubItem url="/partners_new" name="Partner" />
-      <LinkSubItem url="/volunteers_new" name="Volunteer" />
-    </GridItem>
+        <GridItem md={4}>
+            <FooterItemTitle>Work with us</FooterItemTitle>
+            <LinkSubItem url="/partners_new" name="Partner" />
+            <LinkSubItem url="/volunteers_new" name="Volunteer" />
+        </GridItem>
 
-    <GridItem md={1} display="flex" flexDirection="row">
-      <Box
-        sx={{
-          marginLeft: 'auto',
-          alignSelf: 'flex-end',
-        }}
-      >
-        <a href="https://www.linkedin.com/company/open-seattle/">
-          <LinkedInIcon
-            sx={{ color: theme.palette.primary.contrastText }}
-            fontSize="large"
-          />
-        </a>
-      </Box>
-    </GridItem>
+        <GridItem md={1} display="flex" flexDirection="row">
+            <Box
+                sx={{
+                    marginLeft: 'auto',
+                    alignSelf: 'flex-end',
+                }}
+            >
+                <a href="https://www.linkedin.com/company/open-seattle/">
+                    <LinkedInIcon
+                        sx={{ color: theme.palette.primary.contrastText }}
+                        fontSize="large"
+                    />
+                </a>
+            </Box>
+        </GridItem>
 
-    <GridItem md={3}>
-      <SubText>Illustrations by: Storyset by Freepik</SubText>
-      <FooterItemTitle>@ Open Seattle 2023</FooterItemTitle>
-    </GridItem>
+        <GridItem md={3}>
+            <SubText>Illustrations by: Storyset by Freepik</SubText>
+            <FooterItemTitle>@ Open Seattle 2023</FooterItemTitle>
+        </GridItem>
 
-    <GridItem md={4}>
-      <FooterItemTitle>Support us</FooterItemTitle>
-      <FooterSubItem>
-        <a href="mailto:name@email.com">How can you help</a>
-      </FooterSubItem>
-    </GridItem>
+        <GridItem md={4}>
+            <FooterItemTitle>Support us</FooterItemTitle>
+            <FooterSubItem>
+                <a href="mailto:name@email.com">How you can help</a>
+            </FooterSubItem>
+        </GridItem>
 
-    <GridItem md={4}>
-      <FooterItemTitle>About Open Seattle</FooterItemTitle>
-      <LinkSubItem url="/privacy-policy" name="Privacy Policy" />
-    </GridItem>
+        <GridItem md={4}>
+            <FooterItemTitle>About Open Seattle</FooterItemTitle>
+            <LinkSubItem url="/privacy-policy" name="Privacy Policy" />
+        </GridItem>
 
-    <GridItem md={1} display="flex" flexDirection="column">
-      <Box sx={{ alignSelf: 'flex-end' }}>
-        <a href="https://github.com/openseattle">
-          <GitHubIcon
-            sx={{ color: theme.palette.primary.contrastText }}
-            fontSize="large"
-          />
-        </a>
-      </Box>
-    </GridItem>
-  </Grid>
+        <GridItem md={1} display="flex" flexDirection="column">
+            <Box sx={{ alignSelf: 'flex-end' }}>
+                <a href="https://github.com/openseattle">
+                    <GitHubIcon
+                        sx={{ color: theme.palette.primary.contrastText }}
+                        fontSize="large"
+                    />
+                </a>
+            </Box>
+        </GridItem>
+    </Grid>
 )
 
 const CommonFooterMidScreen = () => (
-  <Grid container direction="column" columns={3}>
-    {/* LOGO */}
-    <GridItem md={4}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '100%',
-        }}
-      >
-        <img
-          src={OSLogo.src}
-          style={{
-            height: 'auto',
-            width: '120px',
-          }}
-        />
+    <Grid container direction="column" columns={3}>
+        {/* LOGO */}
+        <GridItem md={4}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                }}
+            >
+                <img
+                    src={OSLogo.src}
+                    style={{
+                        height: 'auto',
+                        width: '120px',
+                    }}
+                />
 
-        <Box>
-          <FooterItemTitle>Contact us</FooterItemTitle>
-          <FooterSubItem>
-            <a href="mailto:name@email.com">info@openseattle.org</a>
-          </FooterSubItem>
-        </Box>
+                <Box>
+                    <FooterItemTitle>Contact us</FooterItemTitle>
+                    <FooterSubItem>
+                        <a href="mailto:name@email.com">info@openseattle.org</a>
+                    </FooterSubItem>
+                </Box>
 
-        <Box>
-          <SubText>Illustrations by: Storyset by Freepik</SubText>
-          <FooterItemTitle>@ Open Seattle 2023</FooterItemTitle>
-        </Box>
-      </Box>
-    </GridItem>
+                <Box>
+                    <SubText>Illustrations by: Storyset by Freepik</SubText>
+                    <FooterItemTitle>@ Open Seattle 2023</FooterItemTitle>
+                </Box>
+            </Box>
+        </GridItem>
 
-    <GridItem md={4}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '100%',
-        }}
-      >
-        <Box>
-          <FooterItemTitle>Work with us</FooterItemTitle>
+        <GridItem md={4}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                }}
+            >
+                <Box>
+                    <FooterItemTitle>Work with us</FooterItemTitle>
 
-          <LinkSubItem url="/partners_new" name="Partner" />
+                    <LinkSubItem url="/partners_new" name="Partner" />
 
-          <LinkSubItem url="/volunteers_new" name="Volunteer" />
-        </Box>
+                    <LinkSubItem url="/volunteers_new" name="Volunteer" />
+                </Box>
 
-        <Box>
-          <FooterItemTitle>Support us</FooterItemTitle>
-          <FooterSubItem>
-            <a href="mailto:name@email.com">How can you help</a>
-          </FooterSubItem>
-        </Box>
+                <Box>
+                    <FooterItemTitle>Support us</FooterItemTitle>
+                    <FooterSubItem>
+                        <a href="mailto:name@email.com">How you can help</a>
+                    </FooterSubItem>
+                </Box>
 
-        <Box>
-          <FooterItemTitle>About Open Seattle</FooterItemTitle>
-          <LinkSubItem url="/privacy-policy" name="Privacy Policy" />
-        </Box>
-      </Box>
-    </GridItem>
+                <Box>
+                    <FooterItemTitle>About Open Seattle</FooterItemTitle>
+                    <LinkSubItem url="/privacy-policy" name="Privacy Policy" />
+                </Box>
+            </Box>
+        </GridItem>
 
-    <GridItem md={4} display="flex">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '2rem',
-        }}
-      >
-        <a href="https://www.linkedin.com/company/open-seattle/">
-          <LinkedInIcon
-            sx={{ color: theme.palette.primary.contrastText }}
-            fontSize="large"
-          />
-        </a>
-        <a href="https://github.com/openseattle">
-          <GitHubIcon
-            sx={{ color: theme.palette.primary.contrastText }}
-            fontSize="large"
-          />
-        </a>
-      </Box>
-    </GridItem>
-  </Grid>
+        <GridItem md={4} display="flex">
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '2rem',
+                }}
+            >
+                <a href="https://www.linkedin.com/company/open-seattle/">
+                    <LinkedInIcon
+                        sx={{ color: theme.palette.primary.contrastText }}
+                        fontSize="large"
+                    />
+                </a>
+                <a href="https://github.com/openseattle">
+                    <GitHubIcon
+                        sx={{ color: theme.palette.primary.contrastText }}
+                        fontSize="large"
+                    />
+                </a>
+            </Box>
+        </GridItem>
+    </Grid>
 )
 
 const CommonFooterSmallScreen = () => (
-  <Grid
-    container
-    direction="column"
-    columns={1}
-    sx={{ alignItems: 'center', textAlign: 'center', gap: '4rem' }}
-  >
-    {/* LOGO */}
-    <GridItem>
-      <img
-        src={OSLogo.src}
-        style={{
-          height: '3rem',
-          width: 'auto',
-        }}
-      />
-    </GridItem>
+    <Grid
+        container
+        direction="column"
+        columns={1}
+        sx={{ alignItems: 'center', textAlign: 'center', gap: '4rem' }}
+    >
+        {/* LOGO */}
+        <GridItem>
+            <img
+                src={OSLogo.src}
+                style={{
+                    height: '3rem',
+                    width: 'auto',
+                }}
+            />
+        </GridItem>
 
-    <GridItem>
-      <FooterItemTitle>Work with us</FooterItemTitle>
-      <LinkSubItem url="/partners_new" name="Partner" />
-      <LinkSubItem url="/volunteers_new" name="Volunteer" />
-    </GridItem>
+        <GridItem>
+            <FooterItemTitle>Work with us</FooterItemTitle>
+            <LinkSubItem url="/partners_new" name="Partner" />
+            <LinkSubItem url="/volunteers_new" name="Volunteer" />
+        </GridItem>
 
-    <GridItem>
-      <FooterItemTitle>Contact us</FooterItemTitle>
-      <FooterSubItem>
-        <a href="mailto:name@email.com">info@openseattle.org</a>
-      </FooterSubItem>
-    </GridItem>
+        <GridItem>
+            <FooterItemTitle>Contact us</FooterItemTitle>
+            <FooterSubItem>
+                <a href="mailto:name@email.com">info@openseattle.org</a>
+            </FooterSubItem>
+        </GridItem>
 
-    <GridItem>
-      <FooterItemTitle>Support us</FooterItemTitle>
-      <FooterSubItem>
-        <a href="mailto:name@email.com">How can you help</a>
-      </FooterSubItem>
-    </GridItem>
+        <GridItem>
+            <FooterItemTitle>Support us</FooterItemTitle>
+            <FooterSubItem>
+                <a href="mailto:name@email.com">How you can help</a>
+            </FooterSubItem>
+        </GridItem>
 
-    <GridItem>
-      <FooterItemTitle>About Open Seattle</FooterItemTitle>
-      <LinkSubItem url="/privacy-policy" name="Privacy Policy" />
-    </GridItem>
+        <GridItem>
+            <FooterItemTitle>About Open Seattle</FooterItemTitle>
+            <LinkSubItem url="/privacy-policy" name="Privacy Policy" />
+        </GridItem>
 
-    <GridItem display="flex" sx={{ gap: '1rem' }}>
-      <a href="https://www.linkedin.com/company/open-seattle/">
-        <LinkedInIcon
-          sx={{ color: theme.palette.primary.contrastText }}
-          fontSize="large"
-        />
-      </a>
-      <a href="https://github.com/openseattle">
-        <GitHubIcon
-          sx={{ color: theme.palette.primary.contrastText }}
-          fontSize="large"
-        />
-      </a>
-    </GridItem>
+        <GridItem display="flex" sx={{ gap: '1rem' }}>
+            <a href="https://www.linkedin.com/company/open-seattle/">
+                <LinkedInIcon
+                    sx={{ color: theme.palette.primary.contrastText }}
+                    fontSize="large"
+                />
+            </a>
+            <a href="https://github.com/openseattle">
+                <GitHubIcon
+                    sx={{ color: theme.palette.primary.contrastText }}
+                    fontSize="large"
+                />
+            </a>
+        </GridItem>
 
-    <GridItem display="flex" flexDirection="column" sx={{ gap: '1rem' }}>
-      <SubText>Illustrations by: Storyset by Freepik</SubText>
-      <FooterItemTitle>@ Open Seattle 2023</FooterItemTitle>
-    </GridItem>
-  </Grid>
+        <GridItem display="flex" flexDirection="column" sx={{ gap: '1rem' }}>
+            <SubText>Illustrations by: Storyset by Freepik</SubText>
+            <FooterItemTitle>@ Open Seattle 2023</FooterItemTitle>
+        </GridItem>
+    </Grid>
 )
 
 const CommonFooter = () => {
-  return (
-    <>
-      <Container
-        sx={{
-          padding: '5rem 0rem',
-          display: {
-            lg: 'flex',
-            xs: 'none',
-          },
-        }}
-      >
-        <CommonFooterLargeScreen />
-      </Container>
+    return (
+        <>
+            <Container
+                sx={{
+                    padding: '5rem 0rem',
+                    display: {
+                        lg: 'flex',
+                        xs: 'none',
+                    },
+                }}
+            >
+                <CommonFooterLargeScreen />
+            </Container>
 
-      <Container
-        sx={{
-          padding: '4rem 2rem',
-          display: { xs: 'none', md: 'flex', lg: 'none' },
-          height: '29rem',
-        }}
-      >
-        <CommonFooterMidScreen />
-      </Container>
+            <Container
+                sx={{
+                    padding: '4rem 2rem',
+                    display: { xs: 'none', md: 'flex', lg: 'none' },
+                    height: '29rem',
+                }}
+            >
+                <CommonFooterMidScreen />
+            </Container>
 
-      <Container
-        sx={{ padding: '4rem 0rem', display: { xs: 'flex', md: 'none' } }}
-      >
-        <CommonFooterSmallScreen />
-      </Container>
-    </>
-  )
+            <Container
+                sx={{ padding: '4rem 0rem', display: { xs: 'flex', md: 'none' } }}
+            >
+                <CommonFooterSmallScreen />
+            </Container>
+        </>
+    )
 }
 export default CommonFooter
