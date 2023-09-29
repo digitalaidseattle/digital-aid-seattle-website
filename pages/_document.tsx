5640e6af5ba04e2659f1cad35914a764f7283426
Yulia Balenko <yulia@YuliaBookPro13.local>
2023-09-28 21:24:20 -0700

    Added favicon to be displayed on the Chrome and Firefox tab

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Oswald:wght@400;500&display=swap"
         />
+        <link
+          rel="icon"
+          sizes="any"
+          href="/logo-light-icon.svg"
+          type="image/svg+xml"
+        />
       </Head>
       <body className="bg-gray-100 text-black dark:bg-gray-700 dark:text-gray-100">
         <Main />
