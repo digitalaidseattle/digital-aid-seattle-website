86815ad7274c5328c9900460dadfb5bc767e119c
jnakaso <jnakaso@yahoo.com>
2023-10-24 18:36:28 -0700

    sanity cleanup

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,35 +1,9 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
-import { homePageQuery, pagePaths, pagesBySlugQuery, teamMemberQuery } from './queries'
-import { TeamMember } from 'types'
+import { apiVersion, dataset, projectId } from '../env'
 
 export const sanityClient = (token?: string, useCdn?: boolean) => {
   return projectId
     ? createClient({ projectId, dataset, apiVersion, useCdn, token })
     : null
-}
-
-export async function getPageBySlug({
-  slug,
-  token,
-}: {
-  slug: string
-  token?: string
-}) {
-  return await sanityClient(token)?.fetch(pagesBySlugQuery, {
-    slug: slug || 'home',
-  })
-}
-
-export async function getHomePage() {
-  return await sanityClient()?.fetch(homePageQuery)
-}
-
-export async function getPagePaths(): Promise<string[] | undefined> {
-  return await sanityClient()?.fetch(pagePaths)
-}
-
-export async function getTeamMembers(): Promise<TeamMember[]> {
-  return await sanityClient()?.fetch(teamMemberQuery)
 }
