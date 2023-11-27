ea2d78813d32d3923d0d7df8229caa08c4992203
Weston Norwood <weston.norwood@gmail.com>
2023-11-26 23:36:05 -0800

    refactor: Add typechecking method for data fields and abstract api call for reuse

--- pages/api/VolunteerRoleService.ts
+++ pages/api/VolunteerRoleService.ts
@@ -2,8 +2,9 @@
  * @2023 Digital Aid Seattle
  */
 
-import Airtable from 'airtable'
 import { DASVolunteerRole, DASVolunteerRoleBasicInfo } from 'types'
+
+import airtableService from './AirtableService'
 
 // Documentation: Airtable provides an excellent guide to using their API.
 // To access documentation for our unique base, visit: https://airtable.com/{{baseID}}/api/docs#javascript/table:roles
@@ -12,176 +13,132 @@ import { DASVolunteerRole, DASVolunteerRoleBasicInfo } from 'types'
 
 
 class DASVolunteerRoleService {
-  private airtable: Airtable
-  private base: Airtable.Base
-  private volunteerRolesTable: Airtable.Table<Airtable.FieldSet>
-  private toolsWeUseTable: Airtable.Table<Airtable.FieldSet>
-
-  constructor(apiKey: string, baseId: string, tableId: string) {
-    this.airtable = new Airtable({
-      apiKey: apiKey || process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
-    })
-    this.base = this.airtable.base(
-      baseId || process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID_DAS
-    )
-    this.volunteerRolesTable = this.base(
-      tableId || process.env.NEXT_PUBLIC_AIRTABLE_TABLE_VOLUNTEER_ROLES
-    )
-    this.toolsWeUseTable = this.base(process.env.NEXT_PUBLIC_AIRTABLE_TABLE_TOOLS_WE_USE)
-  }
 
   async getAllActiveRoles(): Promise<DASVolunteerRoleBasicInfo[]> {
-    try {
-      const records = await this.volunteerRolesTable
-        .select({
-          view: 'Grid view',
-        })
-        .all()
+    const volunteerRolesTable =
+      process.env.NEXT_PUBLIC_AIRTABLE_TABLE_VOLUNTEER_ROLES
+    const maxRecords = 100
+    const activeRoleFilter = '{Status} = "Active"'
 
-      const activeRoles = records.filter(
-        (record) => record.get('Status') === 'Active'
-      )
+    const { getStringFieldRecord } = airtableService
 
-      const volunteerRolesData = activeRoles.map((record) => {
-        return {
-          id: record.id,
-          role: String(record.fields.Role),
-          key: String(record.fields.Key),
-        }
-      })
+    const activeRoles = await airtableService.getTableRecords(
+      volunteerRolesTable,
+      maxRecords,
+      activeRoleFilter
+    )
 
-      return volunteerRolesData
-    } catch (err) {
-      console.log(err)
-      throw err
-    }
+    activeRoles.sort((a, b) => {
+      return a.fields.Role.localeCompare(b.fields.Role)
+    })
+
+    const volunteerRolesData = activeRoles.map((record) => {
+      return {
+        id: record.id,
+        role: getStringFieldRecord(record, 'Role'),
+        key: getStringFieldRecord(record, 'Key'),
+      }
+    })
+    return volunteerRolesData
   }
+
   async getRoleDetailsByName(
     roleKey: string
   ): Promise<DASVolunteerRole | null> {
-    try {
-      const recordData = await this.volunteerRolesTable
-        .select({
-          view: 'Grid view',
-          maxRecords: 1,
-          filterByFormula: `{Key} = '${roleKey}'`,
-        })
-        .firstPage()
-      if (recordData.length === 0) {
-        return null
-      }
-      const record = recordData[0]
+    const volunteerRolesTable =
+      process.env.NEXT_PUBLIC_AIRTABLE_TABLE_VOLUNTEER_ROLES
+    const maxRecords = 1
+    const filterByFormula = `{Key} = '${roleKey}'`
 
-      if (record.fields.Status !== 'Active') {
-        return null
-      }
-      let keyTechnologies = null
+    const recordData = await airtableService.getTableRecords(
+      volunteerRolesTable,
+      maxRecords,
+      filterByFormula
+    )
+    if (recordData.length === 0) {
+      return null
+    }
+    const record = recordData[0];
+    if (record.fields.Status !== 'Active') {
+      return null
+    } else {
+      let keyTechnologies = await this.getAndGroupTechnologies(
+        record.fields['Key technologies']
+      )
 
-      if (record.fields['Key technologies'] && Array.isArray(record.fields['Key technologies'])) {
-        const technologiesRecordIds:Array<any> = record.fields['Key technologies']
-        try {
-          const technologiesData = await this.toolsWeUseTable
-            .select({
-              view: 'Grid view',
-            })
-            .all()
-          const roleTechnologies = technologiesData.filter((tech) => {
-            return technologiesRecordIds.includes(tech.id)
-          })
-          const technologiesMap = new Map()
-          roleTechnologies.forEach((tech) => {
-            const category = tech.fields?.Category || 'Other'
-            const name = tech.fields?.['Name']
-            if (!technologiesMap.has(category)) {
-              technologiesMap.set(category, [])
-            }
-            technologiesMap.get(category).push(name)
-          })
-          keyTechnologies = []
-          technologiesMap.forEach((value, key) => {
-            keyTechnologies.push(`${key}: ${value.join(', ')}`)
-          })
-          keyTechnologies = keyTechnologies.sort((a, b) => {
-            if (a.startsWith('Other')) {
-              return 1
-            } else if (b.startsWith('Other')) {
-              return -1
-            } else {
-            }
-          })
-        } catch (error) {}
-      }
+      const {
+        getNumberFieldRecord,
+        getSplitFieldRecord,
+        getStringFieldRecord,
+      } = airtableService;
+
       const volunteerRoleData: DASVolunteerRole = {
-        role:
-          typeof record.fields.Role === 'string' ? record.fields.Role : null,
-        key: typeof record.fields.Key,
-        location:
-          typeof record.fields?.Location === 'string'
-            ? record.fields?.Location
-            : null,
-        duration:
-          typeof record.fields?.Duration === 'string'
-            ? record.fields?.Duration
-            : null,
-        headline:
-          typeof record.fields?.Headline === 'string'
-            ? record.fields?.Headline
-            : null,
-        description:
-          typeof record.fields?.Description === 'string'
-            ? record.fields?.Description
-            : null,
-        whyJoin:
-          typeof record.fields?.['Why Join Us'] === 'string'
-            ? record.fields?.['Why Join Us']
-            : null,
-        aboutUs:
-          typeof record.fields?.['About Us'] === 'string'
-            ? record.fields?.['About Us']
-            : null,
-        responsibilities:
-          typeof record.fields?.Responsibilities === 'string'
-            ? record.fields?.Responsibilities.split('\n')
-            : null,
-        preferredQualifications:
-          typeof record.fields?.['Preferred Qualifications'] === 'string'
-            ? record.fields?.['Preferred Qualifications']
-            : null,
-        keyAttributesToSuccess:
-          typeof record.fields?.['Key attributes for success'] === 'string'
-            ? record.fields?.['Key attributes for success'].split('\n')
-            : null,
+        role: getStringFieldRecord(record, 'Role'),
+        key: getStringFieldRecord(record, 'Key'),
+        location: getStringFieldRecord(record, 'Location'),
+        duration: getStringFieldRecord(record, 'Duration'),
+        headline: getStringFieldRecord(record, 'Headline'),
+        description: getStringFieldRecord(record, 'Description'),
+        whyJoin: getStringFieldRecord(record, 'Why Join Us'),
+        aboutUs: getStringFieldRecord(record, 'About Us'),
+        responsibilities: getSplitFieldRecord(record, 'Responsibilities'),
+        preferredQualifications: getStringFieldRecord(
+          record,
+          'Preferred Qualifications'
+        ),
+        keyAttributesToSuccess: getSplitFieldRecord(
+          record,
+          'Key attributes for success'
+        ),
         keyTechnologies: keyTechnologies,
-        venture:
-          typeof record.fields?.['Venture (from Partner Needs Link)'] ===
-          'string'
-            ? record.fields?.['Venture (from Partner Needs Link)']
-            : null,
-        applicationLink:
-          typeof record.fields?.['url to apply'] === 'string'
-            ? record.fields?.['url to apply']
-            : null,
-        urgency:
-          typeof record.fields?.Urgency === 'number'
-            ? record.fields?.Urgency
-            : null,
+        venture: getStringFieldRecord(
+          record,
+          'Venture (from Partner Needs Link)'
+        ),
+        applicationLink: getStringFieldRecord(record, 'url to apply'),
+        urgency: getNumberFieldRecord(record, 'Urgency'),
       }
       return volunteerRoleData
-    } catch (error) {
-      console.log(error)
-      throw error
+    }
+  }
+
+  async getAndGroupTechnologies(roleTechnologyRecordIds: string[]) {
+    if (roleTechnologyRecordIds && Array.isArray(roleTechnologyRecordIds)) {
+      const keyTechnologiesData = await airtableService.getTableRecords(
+        process.env.NEXT_PUBLIC_AIRTABLE_TABLE_TOOLS_WE_USE
+      )
+      const roleTechnologies = keyTechnologiesData.filter((tech) => {
+        return roleTechnologyRecordIds.includes(tech.id)
+      })
+      const technologiesMap = new Map()
+      roleTechnologies.forEach((tech) => {
+        const category = tech.fields?.Category || 'Other'
+        const name = tech.fields?.['Name']
+        if (!technologiesMap.has(category)) {
+          technologiesMap.set(category, [])
+        }
+        technologiesMap.get(category).push(name)
+      })
+      let keyTechnologies = []
+      technologiesMap.forEach((value, key) => {
+        keyTechnologies.push(`${key}: ${value.join(', ')}`)
+      })
+      keyTechnologies = keyTechnologies.sort((a, b) => {
+        if (a.startsWith('Other')) {
+          return 1
+        } else if (b.startsWith('Other')) {
+          return -1
+        } else {
+          a.localeCompare(b)
+        }
+      })
+      return keyTechnologies
+    } else {
+      return null
     }
   }
 }
 
-const apiKey = process.env.NEXT_PUBLIC_AIRTABLE_API_KEY
-const baseId = process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID_DAS
-const tableId = process.env.NEXT_PUBLIC_AIRTABLE_TABLE_VOLUNTEER_ROLES
-
-const dasVolunteerRoleService = new DASVolunteerRoleService(
-  apiKey,
-  baseId,
-  tableId
-)
+const dasVolunteerRoleService = new DASVolunteerRoleService()
 
 export { dasVolunteerRoleService }
