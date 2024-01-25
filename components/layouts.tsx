fc3dc0b082f40a78c4ae2bf95ca09091e390446c
Jeffrey Nakasone <jnakaso@yahoo.com>
2024-01-25 12:38:09 -0800

    added loading context (#118)
    * added loading context
    * lint is so picky
    * linear
    * use useState

--- components/layouts.tsx
+++ components/layouts.tsx
@@ -5,6 +5,9 @@ import { theme } from 'theme/theme'
 
 import CommonFooter from './CommonFooter'
 import CommonHeader from './CommonHeader'
+import { ReactNode, createContext, useContext, useState } from 'react'
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
+import { Box, CircularProgress, LinearProgress } from '@mui/material'
 
 // bottom padding is to compensate for footer
 // added background for now to override the default tailwind..
@@ -20,6 +23,39 @@ export const FooterContainer = styled.footer`
   width: 100%;
 `
 
+export const LoadingContext = createContext({
+  loading: false,
+  setLoading: (b: boolean) => { },
+})
+
+const LoadingIndicator = (): ReactJSXElement => {
+  const { loading, setLoading } = useContext(LoadingContext);
+
+  // creating an overlay effect
+  return (loading &&
+    <Box sx={{
+      zIndex: 2,
+      position: 'fixed',
+      width: '100%'
+    }}>
+      <LinearProgress color="success" />
+    </Box>
+  )
+}
+
+const WrappdMainContainer = (props: { children: ReactNode }): ReactJSXElement => {
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <LoadingContext.Provider value={{ loading, setLoading }}>
+      <MainContainer>
+        <LoadingIndicator />
+        {props.children}
+      </MainContainer>
+    </LoadingContext.Provider>
+  );
+}
+
 // eslint-disable-next-line react/display-name
 export const withBasicLayout = (Page: () => JSX.Element) => () =>
 (
@@ -34,9 +70,10 @@ export const withBasicLayout = (Page: () => JSX.Element) => () =>
       }}
     />
     <CommonHeader />
-    <MainContainer>
+
+    <WrappdMainContainer>
       <Page />
-    </MainContainer>
+    </WrappdMainContainer>
     <FooterContainer>
       <CommonFooter />
     </FooterContainer>
