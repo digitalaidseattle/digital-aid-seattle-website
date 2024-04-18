a2bc3908a8dc0211621cf08a242ddf345c0bf8da
Misty <60805050+mistyb01@users.noreply.github.com>
2024-04-18 08:58:40 -0700

    mobile slide-out menu (#142)
    * render a mobilemenu component on hamburger click
    
    * adjust text and spacing
    
    * add close icon
    
    * slide out transition
    
    * fix overlap issue
    
    * menu does not appear on large screens
    
    * clean up unused code

--- components/CommonHeader.tsx
+++ components/CommonHeader.tsx
@@ -3,20 +3,21 @@
  */
 /* eslint-disable jsx-a11y/alt-text  */
 /* eslint-disable @next/next/no-img-element */
+import CloseIcon from '@mui/icons-material/Close'
 import MenuIcon from '@mui/icons-material/Menu'
-import { Box, Button } from '@mui/material'
+import { Box, Button, Typography, useMediaQuery } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Container from '@mui/material/Container'
 import IconButton from '@mui/material/IconButton'
 import Link from '@mui/material/Link'
-import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import Toolbar from '@mui/material/Toolbar'
 import { useRouter } from 'next/router'
-import * as React from 'react'
+import { useState } from 'react'
 import { theme } from 'theme/theme'
 
 import OSLogo from '../assets/darkThemeLogo.svg'
+import MobileMenu from './MobileMenu'
 
 const SECTION_TO_PATH = {
   About: '/about',
@@ -39,16 +40,9 @@ const PATH_TO_SECTION = {
 
 
 const CommonHeader = () => {
-  // React states and functions for handling the hamburger menu.
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget)
-  }
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null)
-  }
+  // React states for handling the hamburger menu.
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const smallScreen = useMediaQuery(theme.breakpoints.down('lg'))
 
   const router = useRouter()
 
@@ -57,136 +51,127 @@ const CommonHeader = () => {
   }
 
   return (
-    <AppBar
-      position="sticky"
-      elevation={0}
-      sx={{
-        background: theme.palette.primary.main,
-      }}
-    >
-      <Toolbar sx={{ padding: '0 !important' }}>
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: { xs: 'flex', lg: 'none' },
-            justifyContent: 'space-between',
-            padding: {
-              xs: '1rem 1rem',
-              md: '1rem 2rem',
-              lg: '1.25rem 0',
-            },
-          }}
-        >
-          {/* Hamburger menu when the screen is small. */}
-          {/* LOGO */}
-          <Link href="/">
-            <img
-              src={OSLogo.src}
-              style={{
-                width: '120px',
-              }}
-              alt="Digital Aid Seattle Home"
-            />
-          </Link>
-          <IconButton
-            size="large"
-            aria-label="page-info"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-            sx={{ color: theme.palette.primary.contrastText }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
+    // Containing Box is given AppBar's z-index; ensures it always stays on top.
+    // z-index taken from docs: https://mui.com/material-ui/customization/z-index/
+    <Box sx={{ position: 'sticky', top: 0, zIndex: 1100 }}>
+      <AppBar
+        position="static"
+        elevation={0}
+        sx={{
+          background: theme.palette.primary.main,
+        }}
+      >
+        <Toolbar sx={{ padding: '0 !important' }}>
+          <Box
             sx={{
-              display: { xs: 'block', lg: 'none' },
+              flexGrow: 1,
+              display: { xs: 'flex', lg: 'none' },
+              justifyContent: 'space-between',
+              padding: {
+                xs: '1rem 1rem',
+                md: '1rem 2rem',
+                lg: '1.25rem 0',
+              },
             }}
           >
+            {/* Hamburger menu when the screen is small. */}
+            {/* LOGO */}
+            <Link href="/">
+              <img
+                src={OSLogo.src}
+                style={{
+                  width: '120px',
+                }}
+                alt="Digital Aid Seattle Home"
+              />
+            </Link>
+            <IconButton
+              size="large"
+              aria-label="page-info"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={()=>setShowMobileMenu(!showMobileMenu)}
+              sx={{ color: theme.palette.primary.contrastText }}
+            >
+              {showMobileMenu ? <CloseIcon /> : <MenuIcon />}
+            </IconButton>
+          </Box>
+          {/* Menu items that are shown on desktop */}
+          <Container
+            disableGutters
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', md: 'none', lg: 'flex' },
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: '1.25rem 2.5rem !important',
+            }}
+          >
+            {/* LOGO */}
+            <Link href="/">
+              <img
+                src={OSLogo.src}
+                style={{
+                  width: '120px',
+                }}
+                alt="Digital Aid Seattle Home"
+              />
+            </Link>
+            <nav>
+              <ul>
+                {Object.keys(SECTION_TO_PATH).map((name) => (
+                  <Button
+                    key={name}
+                    variant="contained"
+                    color={isCurrent(name)
+                      ? 'success'
+                      : 'primary'}
+                    disableRipple={true}
+                  >
+                    <Link
+                      sx={{
+                        color: theme.palette.primary.contrastText,
+                        textUnderlineOffset: '0.5rem',
+                        textDecoration: isCurrent(name)
+                          ? 'underline'
+                          : 'none'
+                      }}
+                      href={SECTION_TO_PATH[name]}
+                    >
+                      {name}
+                    </Link>
+                  </Button>
+                ))}
+              </ul>
+            </nav>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      {/* mobile slide-out menu */}
+      {smallScreen &&
+      <Box sx={{ position: 'relative', zIndex: -1 }}>
+        <MobileMenu yTranslate={showMobileMenu ? '0' : '-500px'}> 
             {Object.keys(SECTION_TO_PATH).map((name) => (
               <MenuItem
                 key={name}
-                onClick={handleCloseNavMenu}
                 style={{ borderRadius: '0px' }}
               >
                 <Link
                   underline="hover"
                   sx={{
-                    color: theme.palette.primary.dark,
+                    color: theme.palette.primary.contrastText,
                   }}
                   href={SECTION_TO_PATH[name]}
                 >
-                  {name}
+                  <Typography variant="labelLarge">
+                    {name}
+                  </Typography>
                 </Link>
               </MenuItem>
             ))}
-          </Menu>
-        </Box>
-        {/* Menu items that are shown on desktop */}
-        <Container
-          disableGutters
-          sx={{
-            flexGrow: 1,
-            display: { xs: 'none', md: 'none', lg: 'flex' },
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            padding: '1.25rem 2.5rem !important',
-          }}
-        >
-          {/* LOGO */}
-          <Link href="/">
-            <img
-              src={OSLogo.src}
-              style={{
-                width: '120px',
-              }}
-              alt="Digital Aid Seattle Home"
-            />
-          </Link>
-          <nav>
-            <ul>
-              {Object.keys(SECTION_TO_PATH).map((name) => (
-                <Button
-                  key={name}
-                  onClick={handleCloseNavMenu}
-                  variant="contained"
-                  color={isCurrent(name)
-                    ? 'success'
-                    : 'primary'}
-                  disableRipple={true}
-                >
-                  <Link
-                    sx={{
-                      color: theme.palette.primary.contrastText,
-                      textUnderlineOffset: '0.5rem',
-                      textDecoration: isCurrent(name)
-                        ? 'underline'
-                        : 'none'
-                    }}
-                    href={SECTION_TO_PATH[name]}
-                  >
-                    {name}
-                  </Link>
-                </Button>
-              ))}
-            </ul>
-          </nav>
-        </Container>
-      </Toolbar>
-    </AppBar>
+          </MobileMenu>
+      </Box>}
+    </Box>
   )
 }
 export default CommonHeader
---
+++ components/MobileMenu.tsx
@@ -0,0 +1,38 @@
+/*
+ * Masthead.tsx
+ * @2023 Digital Aid Seattle
+ */
+
+import {
+  Stack,
+  useTheme,
+} from '@mui/material'
+import { ReactNode } from 'react'
+
+type MobileMenuProps = {
+  children: ReactNode;
+  yTranslate: string;
+}
+
+const MobileMenu = ({ children, yTranslate }: MobileMenuProps) => {
+  const theme = useTheme()
+
+  return (
+  <Stack sx={{
+    position: 'absolute',
+    width: '100%',
+    alignItems: 'center',
+    gap: '1.5rem',
+    paddingY: '2.5rem',
+    backgroundColor: theme.palette.primary.main,
+    borderBottom: `2px solid ${theme.palette.text.primary}`,
+    transform: `translateY(${yTranslate})`,
+    transition: 'all 0.66s ease',
+}}>
+    {children}
+  </Stack>
+
+  )
+}
+
+export default MobileMenu;
