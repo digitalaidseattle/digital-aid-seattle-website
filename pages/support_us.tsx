f93c4821e5c0ba4f04b8576457b58ef972574805
jnakaso <jnakaso@yahoo.com>
2024-07-18 11:53:30 -0700

    add feature flag to support page

--- pages/support_us.tsx
+++ pages/support_us.tsx
@@ -1,8 +1,23 @@
+/*
+ * @2024 Digital Aid Seattle
+ */
 import { Typography, useTheme } from '@mui/material'
 import { withBasicLayout } from 'components/layouts'
+import { useEffect } from 'react'
+import { useFeature } from './api/FeatureService'
+import { useRouter } from 'next/router'
 
 const SupportUsPage = () => {
   const theme = useTheme()
+  const { data: supportUs } = useFeature('support-us');
+  const router = useRouter();
+
+  useEffect(() => {
+    if (supportUs !== undefined && supportUs === false) {
+      console.error(`Support Us feature not implemented.`);
+      router.push('/404')
+    }
+  }, [supportUs])
 
   return (
     <>
