624d6c942f49ee72fa5019d9c502ff305a98d728
jnakaso <jnakaso@yahoo.com>
2024-08-23 15:04:33 -0700

    Added people say section

--- components/CommonHeader.tsx
+++ components/CommonHeader.tsx
@@ -196,18 +196,42 @@ const CommonHeader = () => {
             </nav>
           </Container>
         </Toolbar>
+        {/* mobile slide-out menu */}
+        {smallScreen &&
+          <Box sx={{ position: 'relative', zIndex: -1 }}>
+            <MobileMenu yTranslate={showMobileMenu ? '0' : '-500px'}>
+              {Object.keys(SECTION_TO_PATH).map((name) => (
+                <MenuItem
+                  key={name}
+                  style={{ borderRadius: '0px' }}
+                >
+                  <Link
+                    underline="hover"
+                    sx={{
+                      color: theme.palette.primary.contrastText,
+                    }}
+                    href={SECTION_TO_PATH[name]}
+                  >
+                    <Typography variant="labelLarge">
+                      {name}
+                    </Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </MobileMenu>
+          </Box>}
+        {/* dark overlay */}
+        {showMobileMenu && smallScreen &&
+          <Box
+            sx={{
+              width: '100%',
+              height: '100vh',
+              background: 'rgba(0,0,0,0.6)',
+              position: 'absolute',
+              zIndex: '-2'
+            }}
+          ></Box>}
       </AppBar>
-      {/* mobile slide-out menu */}
-      {smallScreen &&
-        <Box sx={{ position: 'relative', zIndex: -1 }}>
-          <MobileMenu yTranslate={showMobileMenu ? '0' : '-500px'}>
-            {menuItems.map((section) => (
-              (section.style === 'primary')
-                ? mobilePrimaryMenuItem(section)
-                : mobileSecondaryMenuItem(section)
-            ))}
-          </MobileMenu>
-        </Box>}
     </Box>
   )
 }
