6491e781e03e38afb7315518c67676e536cd565d
jnakaso <jnakaso@yahoo.com>
2024-10-15 14:04:06 -0700

    mobile footers

--- components/CommonFooter.tsx
+++ components/CommonFooter.tsx
@@ -1,10 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text  */
 /* eslint-disable @next/next/no-img-element */
 
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
 import { Box, Container, Grid, Stack, styled, Typography } from '@mui/material'
 import Link from '@mui/material/Link'
 import { theme } from 'theme/theme'
@@ -13,7 +9,7 @@ import { ReactNode } from 'react'
 import OSLogo from '../assets/darkThemeLogo.svg'
 import EmailFormContainer from './email-form/EmailFormContainer'
 import { useFeature } from 'pages/api/FeatureService'
-import { Facebook, Instagram } from '@mui/icons-material'
+import { GitHub, LinkedIn, Facebook, Instagram } from '@mui/icons-material'
 
 // const GridItem = styled(Grid)(() => {
 //   return {
@@ -24,6 +20,16 @@ import { Facebook, Instagram } from '@mui/icons-material'
 //   }
 // })
 
+const SubText = styled(Typography)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  fontSize: '0.75rem',
+}))
+
+const FooterItemTitle = styled(Typography)(({ theme }) => ({
+  fontWeight: 700,
+  color: theme.palette.primary.contrastText,
+  fontSize: '0.875rem',
+}))
 
 type GridItemProps = {
   children: ReactNode
@@ -52,19 +58,6 @@ const GridItem = ({ md, children }: GridItemProps) =>
     </Grid>
   )
 
-const FooterItemTitle = styled(Typography)(({ theme }) => ({
-  fontWeight: 700,
-  color: theme.palette.primary.contrastText,
-  fontSize: '0.875rem',
-}))
-
-const FooterSubItem = styled(Typography)(({ theme }) => ({
-  color: theme.palette.primary.contrastText,
-  fontSize: '1rem',
-  marginTop: '1rem',
-  display: 'block',
-}))
-
 const LinkSubItem = (props: { url: string; name: string }) => (
   <Link
     sx={{
@@ -79,12 +72,7 @@ const LinkSubItem = (props: { url: string; name: string }) => (
   </Link>
 )
 
-const SubText = styled(Typography)(({ theme }) => ({
-  color: theme.palette.primary.contrastText,
-  fontSize: '0.75rem',
-}))
-
-const logoBox = () => {
+const LogoBox = () => {
   return (
     <Box>
       <Link href="/" title="Go back to home" aria-label="Go back to home">
@@ -98,50 +86,36 @@ const AboutBox = () => {
   const { data: faq } = useFeature('faq')
   return (
     <Box>
-      <FooterItemTitle>About Digital Aid Seattle</FooterItemTitle>
+      <FooterItemTitle>About Us</FooterItemTitle>
       <LinkSubItem url="/privacy" name="Privacy Policy" />
       {faq && <LinkSubItem url="/faq" name="FAQ" />}
     </Box>
   )
 }
 
-const contactUsBox = () => {
+const ContactUsBox = () => {
   return (
     <Box>
       <FooterItemTitle>Contact us</FooterItemTitle>
-      <FooterSubItem>
-        <a href="mailto:info@digitalaidseattle.org">
-          info@digitalaidseattle.org
-        </a>
-      </FooterSubItem>
+      <LinkSubItem url="mailto:info@digitalaidseattle.org" name="info@digitalaidseattle.org" />
     </Box>
   )
 }
 
-const workWithUsBox = () => {
+const WorkWithUsBox = () => {
   return (
     <Box>
       <FooterItemTitle>Work with us</FooterItemTitle>
       <LinkSubItem url="/partners" name="Partner" />
       <LinkSubItem url="/volunteers" name="Volunteer" />
+      <LinkSubItem url="/support_us" name="Donate" />
     </Box>
   )
 }
 
-const SupportUsBox = () => {
-  const { data: supportUs } = useFeature('support-us')
-  return (supportUs &&
-    <Box>
-      <FooterItemTitle>Support us</FooterItemTitle>
-      <FooterSubItem>
-        <a href="mailto:info@digitalaidseattle.org">Donate</a>
-      </FooterSubItem>
-    </Box>)
-}
-
-const copyrightBox = () => {
+const CopyrightBox = () => {
   return (
-    <Stack gap="0.25rem">
+    <Stack gap="1rem">
       <SubText>
         Illustrations by{' '}
         <Link
@@ -168,7 +142,7 @@ const linkedInLink = () => {
       title="LinkedIn"
       aria-label="Our LinkedIn, which opens in a new window."
     >
-      <LinkedInIcon
+      <LinkedIn
         sx={{ color: theme.palette.primary.contrastText }}
         fontSize="large"
       />
@@ -185,7 +159,7 @@ const githubLink = () => {
       title="GitHub"
       aria-label="Our GitHub, which opens in a new window."
     >
-      <GitHubIcon
+      <GitHub
         sx={{ color: theme.palette.primary.contrastText }}
         fontSize="large"
       />
@@ -202,7 +176,7 @@ const facebookLink = () => {
       title="Facebook"
       aria-label="Our Facebook page, which opens in a new window."
     >
-      <FacebookIcon
+      <Facebook
         sx={{ color: theme.palette.primary.contrastText }}
         fontSize="large"
       />
@@ -219,7 +193,7 @@ const instagramLink = () => {
       title="Instagram"
       aria-label="Our Instagram page, which opens in a new window."
     >
-      <InstagramIcon
+      <Instagram
         sx={{ color: theme.palette.primary.contrastText }}
         fontSize="large"
       />
@@ -235,204 +209,140 @@ const START_BOX_STYLE = {
   gap: '2rem',
 }
 
-const CommonFooterLargeScreen = () => (
-  <Grid container spacing={5}>
-    <GridItem md={4}>
+const CommonFooterLargeScreen = () => {
+  const { data: newsLetter } = useFeature('newsletter')
+
+  return (
+    <Stack>
+      {newsLetter && <EmailFormContainer />}
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '2rem',
+          paddingY: '5rem',
+          height: '28rem'
         }}
       >
-        {logoBox()}
-      </Box>
-    </GridItem>
+        <Grid container spacing={4}>
+          <GridItem md={4}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '2rem',
+              }}
+            >
+              <LogoBox />
+            </Box>
+          </GridItem>
 
-    <GridItem md={4}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}
-      >
-        {workWithUsBox()}
-      </Box>
-    </GridItem>
+          <GridItem md={4}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              <WorkWithUsBox />
+            </Box>
+          </GridItem>
 
-    <GridItem md={4}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}
-      >
-        <AboutBox />
-      </Box>
-    </GridItem>
+          <GridItem md={4}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              <AboutBox />
+            </Box>
+          </GridItem>
 
-    <GridItem md={4}>
-      <Box sx={START_BOX_STYLE}>
-      </Box>
-    </GridItem>
-    <GridItem md={4}>
-      <Box sx={START_BOX_STYLE}>
-        <SupportUsBox />
-      </Box>
-    </GridItem>
+          <GridItem md={4}>
+            <Box sx={START_BOX_STYLE}>
+            </Box>
+          </GridItem>
 
-    <GridItem md={4}>
-      <Box sx={START_BOX_STYLE}>
-        {contactUsBox()}
-      </Box>
-    </GridItem>
+          <GridItem md={4}>
+            <Box sx={START_BOX_STYLE}>
+              <ContactUsBox />
+            </Box>
+          </GridItem>
 
-    <GridItem md={4}>
-      <Box sx={START_BOX_STYLE}>
-        {copyrightBox()}
+          <GridItem md={4}>
+            <Box sx={START_BOX_STYLE}>
+            </Box>
+          </GridItem>
+
+          <GridItem md={4}>
+            <Box sx={START_BOX_STYLE}>
+              <CopyrightBox />
+            </Box>
+          </GridItem>
+          <GridItem md={8}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '2rem',
+                alignItems: 'flex-end',
+              }}
+            >
+              <Box >
+                <FooterItemTitle>Follow us on social media</FooterItemTitle>
+                <Stack direction={'row'} marginTop={1} spacing={2}>
+                  {linkedInLink()}
+                  {githubLink()}
+                  {facebookLink()}
+                  {instagramLink()}
+                </Stack>
+              </Box>
+            </Box>
+          </GridItem>
+        </Grid>
       </Box>
-    </GridItem>
-    <GridItem md={8}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          gap: '2rem',
-          alignItems: 'flex-end',
-        }}
-      >
-        <Box >
-          <FooterItemTitle>Follow us on social media</FooterItemTitle>
-          <FooterSubItem>
-            <Stack direction={'row'} spacing={2}>
-              {linkedInLink()}
-              {githubLink()}
-              {facebookLink()}
-              {instagramLink()}
-            </Stack>
-          </FooterSubItem>
+    </Stack>)
+}
+
+const CommonFooterSmallScreen = () => {
+  const { data: newsLetter } = useFeature('newsletter')
+
+  return (
+    <Grid
+      container
+      direction="column"
+      columns={1}
+      sx={{ alignItems: 'center', textAlign: 'center', paddingY: '4rem', gap: '2rem' }}
+    >
+      <GridItem><LogoBox /></GridItem>
+      {newsLetter && <EmailFormContainer />}
+      <GridItem><WorkWithUsBox /></GridItem>
+      <GridItem><AboutBox /></GridItem>
+      <GridItem><ContactUsBox /></GridItem>
+      <GridItem>
+        <FooterItemTitle>Follow us on social media</FooterItemTitle>
+        <Box display="flex" sx={{ gap: '1rem', marginTop: '1rem' }}>
+          {linkedInLink()}
+          {githubLink()}
+          {facebookLink()}
+          {instagramLink()}
         </Box>
-      </Box>
-    </GridItem>
-  </Grid>
-)
-
-const CommonFooterMidScreen = () => (
-  <Grid container direction="column" columns={3}>
-    <GridItem md={5}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          height: '100%',
-          gap: '2rem',
-        }}
-      >
-        {logoBox()}
-        {copyrightBox()}
-      </Box>
-    </GridItem>
-
-    <GridItem md={5}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '100%',
-        }}
-      >
-        {contactUsBox()}
-        {workWithUsBox()}
-        <AboutBox />
-      </Box>
-    </GridItem>
-
-    <GridItem md={2}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '2rem',
-          alignItems: 'flex-end',
-        }}
-      >
-        {linkedInLink()}
-        {githubLink()}
-        {facebookLink()}
-        {instagramLink()}
-      </Box>
-    </GridItem>
-  </Grid>
-)
-
-const CommonFooterSmallScreen = () => (
-  <Grid
-    container
-    direction="column"
-    columns={1}
-    sx={{ alignItems: 'center', textAlign: 'center', gap: '2rem' }}
-  >
-    <GridItem>{logoBox()}</GridItem>
-
-    <GridItem>{contactUsBox()}</GridItem>
-
-    <GridItem>{workWithUsBox()}</GridItem>
-
-    <GridItem><AboutBox /></GridItem>
-
-    <GridItem>
-      <Box display="flex" sx={{ gap: '1rem' }}>
-        {linkedInLink()}
-        {githubLink()}
-        {facebookLink()}
-        {instagramLink()}
-      </Box>
-    </GridItem>
-
-    <GridItem>{copyrightBox()}</GridItem>
-  </Grid>
-)
+      </GridItem>
+      <GridItem><CopyrightBox /></GridItem>
+    </Grid>
+  )
+}
 
 const CommonFooter = () => {
-  const { data: newsLetter } = useFeature('newsletter')
   return (
-    <>
-      <Container>
-        {newsLetter && <EmailFormContainer />}
-        <Box
-          sx={{
-            paddingY: '5rem',
-            display: { xs: 'none', md: 'none', lg: 'flex' },
-            height: '28rem',
-          }}
-        >
-          <CommonFooterLargeScreen />
-        </Box>
-
-        <Box
-          sx={{
-            paddingY: '4rem',
-            display: { xs: 'none', md: 'flex', lg: 'none' },
-            height: '29rem',
-          }}
-        >
-          <CommonFooterMidScreen />
-        </Box>
-
-        <Box
-          sx={{
-            paddingY: '4rem',
-            display: { xs: 'flex', md: 'none', lg: 'none' },
-          }}
-        >
-          <CommonFooterSmallScreen />
-        </Box>
-      </Container>
-    </>
+    <Container>
+      <Box sx={{ display: { xs: 'none', md: 'flex', lg: 'flex' } }}>
+        <CommonFooterLargeScreen />
+      </Box>
+      <Box sx={{ display: { xs: 'flex', md: 'none', lg: 'none' } }}>
+        <CommonFooterSmallScreen />
+      </Box>
+    </Container >
   )
 }
 export default CommonFooter
--- components/email-form/EmailFormContainer.tsx
+++ components/email-form/EmailFormContainer.tsx
@@ -19,61 +19,93 @@ const EmailFormContainer = () => {
     }
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-    <Stack
-      sx={{
-        paddingTop: { xs: '4rem', lg: '5rem' },
-        flexDirection: { xs: 'column', lg: 'row' },
-        alignItems: { xs: 'flex-start', lg: 'center' },
-        gap: '2rem',
-      }}
-      justifyContent={'space-between'}
-    >
-      <Stack direction={'column'}>
+  const LabelSection = () => {
+    return (
+      <Stack direction={'column'}
+        sx={{
+          width: { xs: '100%' }
+        }}
+      >
         <Typography
           variant="headlineMedium"
           sx={{ color: theme.palette.primary.contrastText }}
         >
-          Sign up for our newsletter
+          Stay Connected!
         </Typography>
         <Typography
           variant="bodyLarge"
           sx={{ color: theme.palette.primary.contrastText }}
         >
-          Stay in the know!
+          Sign up for our newsletter
         </Typography>
       </Stack>
-        <Stack gap='1rem' sx={{width: { xs: '100%', lg: 'auto' }}}>
-        <Stack
-          gap='2rem'
-          sx={{
-            flexDirection: { xs: 'column', md: 'row' },
-          }}
-        >
-            <EmailFormInput 
+    )
+  }
+
+
+
+  const InputSection = () => {
+    return (
+      <Stack
+        sx={{
+          width: { xs: '100%', lg: 'auto' },
+          flexDirection: { xs: 'column', md: 'row', lg: 'row' },
+        }}>
+        <Stack>
+          <Stack gap='1rem'
+            sx={{
+              width: { xs: '100%', lg: 'auto' },
+              flexDirection: { xs: 'column', md: 'row' },
+            }}>
+            <EmailFormInput
               userEmail={email}
-              handleEmailInput={(formInput)=>setEmail(formInput)}
+              handleEmailInput={(formInput) => setEmail(formInput)}
               error={error}
             />
+            {error &&
+              <Typography
+                sx={{
+                  display: { xs: 'flex', md: 'none', lg: 'none' },
+                  paddingLeft: '1rem',
+                  color: theme.palette.error.main,
+                }}>Error: {error}</Typography>
+            }
             <Button
-              type="submit"
+              variant="contained"
               sx={{
                 color: theme.palette.primary.contrastText,
                 backgroundColor: theme.palette.primary.dark,
               }}
+              onClick={handleSubmit}
             >
               Sign up
             </Button>
+          </Stack>
+          {error &&
+            <Typography
+              sx={{
+                display: { xs: 'none', md: 'flex', lg: 'flex' },
+                paddingLeft: '1rem',
+                color: theme.palette.error.main,
+              }}>Error: {error}</Typography>
+          }
         </Stack>
-        {error && <Typography sx={{
-          paddingLeft: '1rem',
-          color: theme.palette.error.main, 
-          }}>Error: {error}</Typography>}
       </Stack>
-    </Stack>
-    </form>
+    )
+  }
 
+  return (
+    <Stack
+      sx={{
+        width: { xs: '80%', lg: 'auto' },
+        paddingTop: { md: '5rem', lg: '5rem' },
+        flexDirection: { xs: 'column', md: 'column', lg: 'row' },
+        alignItems: { xs: 'flex-start', md: 'center', lg: 'center' }
+      }}
+    >
+      <LabelSection />
+      <InputSection />
+    </Stack>
   )
 }
 
