ddf6d1a8810ca913cac31fec42568abc83b5489a
jnakaso <jnakaso@yahoo.com>
2024-11-26 15:02:19 -0800

    brevo

--- app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-// To stream responses you must use Route Handlers in the App Router, even if the rest of your app uses the Pages Router.
-
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
-
-export function GET(request: Request) {
-  console.log('reaching hello', request)
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
-}
--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,8 +1,13 @@
+/**
+* newsletter.ts
+* 
+* serverless function to connect with Brevo
+* 
+* @2024 Digital Aid Seattle
+*/
 
-// To stream responses you must use Route Handlers in the App Router, even if the rest of your app uses the Pages Router.
-// import Brevo from '@getbrevo/brevo';
-// import { ContactsApiApiKeys } from '@getbrevo/brevo';
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import Brevo, { ContactsApiApiKeys } from '@getbrevo/brevo';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   console.log('reaching handler:', req);
@@ -13,13 +18,13 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     console.log('Received data:', data);
     console.log('Received data:', JSON.stringify(data));
 
-    // let contactsClient = new Brevo.ContactsApi();
-    // contactsClient.setApiKey(ContactsApiApiKeys.apiKey, process.env.NEXT_PUBLIC_BREVO_API_KEY!);
+    let contactsClient = new Brevo.ContactsApi();
+    contactsClient.setApiKey(ContactsApiApiKeys.apiKey, process.env.NEXT_PUBLIC_BREVO_API_KEY!);
 
-    // const contact = new Brevo.CreateContact();
-    // contact.email = data.email;
-    // contact.listIds = data.listIds;
-    // const response = await contactsClient.createContact(contact)
+    const contact = new Brevo.CreateContact();
+    contact.email = data.email;
+    contact.listIds = data.listIds;
+    const response = await contactsClient.createContact(contact)
     res.status(200).json({ message: 'Data posted successfully', data });
   } else {
     res.status(405).json({ error: `Method not allowed: ${req.method}` });
