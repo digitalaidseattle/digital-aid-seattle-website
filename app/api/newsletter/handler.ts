8e4926479e869eda8d836171b9355f5c0a5a2bb3
jnakaso <jnakaso@yahoo.com>
2024-11-26 13:09:47 -0800

    vercel post

---
+++ app/api/newsletter/handler.ts
@@ -0,0 +1,25 @@
+
+// To stream responses you must use Route Handlers in the App Router, even if the rest of your app uses the Pages Router.
+import Brevo from '@getbrevo/brevo';
+import { ContactsApiApiKeys } from '@getbrevo/brevo';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === 'POST') {
+    const data = req.body; // Access the POST data
+
+    // Process the data
+    console.log('Received data:', data);
+
+    let contactsClient = new Brevo.ContactsApi();
+    contactsClient.setApiKey(ContactsApiApiKeys.apiKey, process.env.NEXT_PUBLIC_BREVO_API_KEY!);
+
+    const contact = new Brevo.CreateContact();
+    contact.email = data.email;
+    contact.listIds![2];
+    const response = await contactsClient.createContact(contact)
+    res.status(200).json({ message: 'Data posted successfully', response });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
--- pages/api/EmailService.ts
+++ pages/api/EmailService.ts
@@ -4,6 +4,8 @@
 */
 
 const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+// let contactsClient = new Brevo.ContactsApi();
+// contactsClient.setApiKey(ContactsApiApiKeys.apiKey, process.env.NEXT_PUBLIC_BREVO_API_KEY);
 
 class EmailService {
 
@@ -11,11 +13,28 @@ class EmailService {
         return email.match(EMAIL_REGEX) != null
     }
 
-    async subscribe(email: string): Promise<boolean> {
-        // Call API to subscribe
-        return true
+    async subscribe(email: string): Promise<any> {
+        return fetch('https://api.brevo.com/v3/contacts', {
+            method: 'POST',
+            headers: {
+                'api-key': process.env.NEXT_PUBLIC_BREVO_API_KEY, // Use a backend to keep this secure
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                listIds: [2],
+            }),
+        })
+            .then((response) => response.json())
+
+        return true;
+        // const contact = new Brevo.CreateContact();
+        // contact.email = email;
+        // contact.listIds[2];
+        // return contactsClient.createContact(contact)
     }
 }
 
 const emailService = new EmailService();
 export { emailService };
+
