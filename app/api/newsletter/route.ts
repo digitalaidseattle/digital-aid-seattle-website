2457a0466960ab0472210ea3df7f542dd55b3ba4
jnakaso <jnakaso@yahoo.com>
2024-11-26 13:52:15 -0800

    handler

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,7 +1,12 @@
 // To stream responses you must use Route Handlers in the App Router, even if the rest of your app uses the Pages Router.
 
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export function POST(request: Request) {
-  return new Response(`Hello from ${request.body}`);
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === 'POST') {
+    const jsonData = req.body; // req.body is already parsed
+    res.status(200).json({ parsedData: jsonData });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
 }
