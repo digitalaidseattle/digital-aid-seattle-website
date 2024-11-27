9b7d509b0d74a7488734f94ce51c23022044e7ba
jnakaso <jnakaso@yahoo.com>
2024-11-26 16:36:23 -0800

    default client

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -12,8 +12,10 @@ const SibApiV3Sdk = require('sib-api-v3-sdk');
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
+    let defaultClient = SibApiV3Sdk.ApiClient.instance;
+    let apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = process.env.NEXT_PUBLIC_BREVO_API_KEY!;
     let contactsClient = new SibApiV3Sdk.ContactsApi();
-    contactsClient.setApiKey(SibApiV3Sdk.ContactsApiApiKeys.apiKey, process.env.NEXT_PUBLIC_BREVO_API_KEY!);
 
     const contact = new SibApiV3Sdk.CreateContact();
     contact.email = req.body.email;
