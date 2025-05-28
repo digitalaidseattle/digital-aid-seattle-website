12bbf40c8717e9217db0587b815f3c13eb9655e9
jnakaso <jnakaso@yahoo.com>
2025-05-27 19:42:32 -0700

    improved error handling

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -12,16 +12,6 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
-    // let defaultClient = SibApiV3Sdk.ApiClient.instance;
-    // let apiKey = defaultClient.authentications['api-key'];
-    // apiKey.apiKey = process.env.NEXT_PUBLIC_BREVO_API_KEY!;
-    // let contactsClient = new SibApiV3Sdk.ContactsApi();
-
-    // const contact = new SibApiV3Sdk.CreateContact();
-    // contact.email = req.body.email;
-    // contact.listIds = req.body.listIds;
-    // const brevoResponse = await contactsClient.createContact(contact);
-
     const { email, attributes = {}, listIds = [] } = req.body;
 
     const brevoResponse = await fetch('https://api.brevo.com/v3/contacts', {
